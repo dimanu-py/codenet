21c4dcc15b8d3e79e4acfa559fe7b655be1a91b6
dimanu-py <diegomtz126@gmail.com>
2025-01-11 00:29:03 +0100

    feat(user): create DTO to capture client request

--- src/delivery/api/user/user_route.py
+++ src/delivery/api/user/user_route.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
+from src.delivery.api.user.user_register_request import RegisterUserRequest
+
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.put("/{id_}")
-async def register_user(id_: str, request: dict) -> JSONResponse:
+async def register_user(id_: str, request: RegisterUserRequest) -> JSONResponse:
 	raise NotImplementedError
