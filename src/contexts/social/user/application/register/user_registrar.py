d0ec2ee346cc66048510fff8831694760defea47
dimanu-py <diegomtz126@gmail.com>
2025-01-12 19:24:03 +0100

    refactor(user): inject repository to use case
    For now is marked as optional to avoid touching route until cycle is finished

--- src/contexts/social/user/application/register/user_registrar.py
+++ src/contexts/social/user/application/register/user_registrar.py
@@ -1,8 +1,14 @@
 from src.contexts.social.user.application.register.register_user_command import (
     RegisterUserCommand,
 )
+from src.contexts.social.user.domain.user_repository import UserRepository
 
 
 class UserRegistrar:
+    _repository: UserRepository | None
+
+    def __init__(self, repository: UserRepository | None = None) -> None:
+        self._repository = repository
+
     def __call__(self, command: RegisterUserCommand) -> None:
         raise NotImplementedError
