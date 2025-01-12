078d2bea3f7a7ba9d31692a8498733f148d84922
dimanu-py <diegomtz126@gmail.com>
2025-01-12 19:26:14 +0100

    feat(user): create User object to model our users
    First iteration with primitives

---
+++ src/contexts/social/user/domain/user.py
@@ -0,0 +1,27 @@
+class User:
+    _id: str
+    _name: str
+    _username: str
+    _email: str
+    _profile_picture: str
+
+    def __init__(
+        self, id_: str, name: str, username: str, email: str, profile_picture: str
+    ) -> None:
+        self._id = id_
+        self._name = name
+        self._username = username
+        self._email = email
+        self._profile_picture = profile_picture
+
+    @classmethod
+    def create(
+        cls, id_: str, name: str, username: str, email: str, profile_picture: str
+    ) -> "User":
+        return User(
+            id_=id_,
+            name=name,
+            username=username,
+            email=email,
+            profile_picture=profile_picture,
+        )
