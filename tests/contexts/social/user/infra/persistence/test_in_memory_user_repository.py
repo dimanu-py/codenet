0c5cd4055707b527095df3ea9263b5fe7de4437a
dimanu-py <diegomtz126@gmail.com>
2025-01-13 22:30:59 +0100

    refactor(user): extract setup method in integration user test

--- tests/contexts/social/user/infra/persistence/test_in_memory_user_repository.py
+++ tests/contexts/social/user/infra/persistence/test_in_memory_user_repository.py
@@ -10,26 +10,30 @@ from tests.contexts.social.user.domain.user_mother import UserMother
 
 @pytest.mark.integration
 class TestInMemoryUserRepository:
+    def setup_method(self) -> None:
+        self._repository = InMemoryUserRepository()
+
     @pytest.mark.asyncio
     async def test_should_save_valid_user(self) -> None:
-        repository = InMemoryUserRepository()
         user = UserMother.create()
 
-        await repository.save(user)
+        await self._repository.save(user)
 
-        saved_user = await repository.search(user.id)
+        saved_user = await self._repository.search(user.id)
         self.assert_users_match(saved_user, user)
-
-    def assert_users_match(self, expected_user: User | None, user: User) -> None:
-        expect(expected_user).to(equal(user))
 
     @pytest.mark.asyncio
     async def test_should_delete_valid_user(self) -> None:
-        repository = InMemoryUserRepository()
         user = UserMother.create()
-        await repository.save(user)
+        await self._repository.save(user)
 
-        await repository.delete(user.id)
+        await self._repository.delete(user.id)
 
-        saved_user = await repository.search(user.id)
-        self.assert_users_match(saved_user, user)
+        saved_user = await self._repository.search(user.id)
+        self.assert_has_not_found(saved_user)
+
+    def assert_users_match(self, expected_user: User | None, user: User | None) -> None:
+        expect(expected_user).to(equal(user))
+
+    def assert_has_not_found(self, user: User | None) -> None:
+        expect(user).to(equal(None))
