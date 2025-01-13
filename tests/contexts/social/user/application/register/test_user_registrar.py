2653bee3e1d41f203610b5e0e0d0d57c680f5f74
dimanu-py <diegomtz126@gmail.com>
2025-01-13 19:42:24 +0100

    refactor(user): move instantiation of repository and user registrar to setup method

--- tests/contexts/social/user/application/register/test_user_registrar.py
+++ tests/contexts/social/user/application/register/test_user_registrar.py
@@ -21,6 +21,7 @@ from src.contexts.social.user.domain.invalid_url_format_error import (
 from src.contexts.social.user.domain.invalid_username_format_error import (
     InvalidUsernameFormatError,
 )
+from src.contexts.social.user.domain.user import User
 from src.contexts.social.user.domain.user_repository import UserRepository
 from tests.contexts.shared.expects.matchers import async_expect, raise_error
 from tests.contexts.social.user.application.register.register_user_command_mother import (
@@ -37,17 +38,19 @@ class TestUserRegistrar:
         future.set_result(result)
         return future
 
+    def setup_method(self) -> None:
+        self._repository = Mock(UserRepository)
+        self._user_registrar = UserRegistrar(repository=self._repository)
+
     @pytest.mark.asyncio
     async def test_should_register_a_valid_user(self) -> None:
         command = RegisterUserCommandMother.create()
         user = UserMother.from_command(command)
-        repository = Mock(UserRepository)
-        user_registrar = UserRegistrar(repository=repository)
-        expect_call(repository).save(user).returns(self._immediate_future())
+        self.should_save_user(user)
 
-        await user_registrar(command)
+        await self._user_registrar(command)
 
-        expect(repository).to(have_been_satisfied)
+        self.assert_has_satisfied_conditions()
 
     @pytest.mark.parametrize(
         "invalid_field, expected_error",
@@ -63,10 +66,14 @@ class TestUserRegistrar:
     async def test_should_not_allow_to_store_an_invalid_user(
         self, invalid_field: dict, expected_error: Exception
     ) -> None:
-        repository = Mock(UserRepository)
-        user_registrar = UserRegistrar(repository=repository)
         command = RegisterUserCommandMother.create(invalid_field)
 
-        await async_expect(lambda: user_registrar(command)).to(
+        await async_expect(lambda: self._user_registrar(command)).to(
             raise_error(expected_error)
         )
+
+    def assert_has_satisfied_conditions(self):
+        expect(self._repository).to(have_been_satisfied)
+
+    def should_save_user(self, user: User) -> None:
+        expect_call(self._repository).save(user).returns(self._immediate_future())
