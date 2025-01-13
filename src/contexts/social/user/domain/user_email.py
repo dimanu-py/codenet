e15ee08299da175ed4cf470dd3d2553cc9296c37
dimanu-py <diegomtz126@gmail.com>
2025-01-13 17:44:25 +0100

    feat(user): create UserEmail value object

---
+++ src/contexts/social/user/domain/user_email.py
@@ -0,0 +1,20 @@
+import re
+
+from src.contexts.shared.domain.value_objects.string_value_object import (
+    StringValueObject,
+)
+from src.contexts.social.user.domain.invalid_email_format_error import (
+    InvalidEmailFormatError,
+)
+
+
+class UserEmail(StringValueObject):
+    EMAIL_FORMAT = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    def _validate(self, value: str) -> None:
+        super()._validate(value)
+        self._ensure_email_has_correct_format(value)
+
+    def _ensure_email_has_correct_format(self, value: str) -> None:
+        if re.match(self.EMAIL_FORMAT, value) is None:
+            raise InvalidEmailFormatError
