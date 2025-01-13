8d6538834ba7b67483b714444ff90783d2a43cd4
dimanu-py <diegomtz126@gmail.com>
2025-01-13 18:32:11 +0100

    refactor(user): use new mothers in UserMother and RegisterUserCommandMother to avoid hardcoding fields

--- tests/contexts/social/user/application/register/register_user_command_mother.py
+++ tests/contexts/social/user/application/register/register_user_command_mother.py
@@ -1,17 +1,24 @@
 from src.contexts.social.user.application.register.register_user_command import (
     RegisterUserCommand,
 )
+from tests.contexts.social.user.domain.user_email_mother import UserEmailMother
+from tests.contexts.social.user.domain.user_full_name_mother import UserFullNameMother
+from tests.contexts.social.user.domain.user_id_mother import UserIdMother
+from tests.contexts.social.user.domain.user_name_mother import UserNameMother
+from tests.contexts.social.user.domain.user_profile_picture_mother import (
+    UserProfilePictureMother,
+)
 
 
 class RegisterUserCommandMother:
     @classmethod
     def create(cls, fixed_values: dict | None = None) -> RegisterUserCommand:
         primitives = {
-            "id": "8a97585f-4d7a-42ba-8d82-ab8da94d2c4a",
-            "name": "John Doe",
-            "username": "john_doe",
-            "email": "johndoe@gmail.com",
-            "profile_picture": "https://my-bucket.s3.us-east-1.amazonaws.com/images/picture.jpg",
+            "id": UserIdMother.create().value,
+            "name": UserFullNameMother.create().value,
+            "username": UserNameMother.create().value,
+            "email": UserEmailMother.create().value,
+            "profile_picture": UserProfilePictureMother.create().value,
             **(fixed_values if fixed_values else {}),
         }
         return RegisterUserCommand(**primitives)
