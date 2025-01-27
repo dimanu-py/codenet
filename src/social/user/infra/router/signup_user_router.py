f2e0f4a689c202a2ee3c238da0ccf43061c4c793
dimanu-py <diegomtz126@gmail.com>
2025-01-27 18:42:27 +0100

    ✨ feat(user): design interaction between controller and bussiness logic

--- src/social/user/infra/router/signup_user_router.py
+++ src/social/user/infra/router/signup_user_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, status
 
+from src.social.user.application.signup.user_signup import UserSignup
+from src.social.user.domain.user_repository import UserRepository
 from src.social.user.infra.router.user_sign_up_request import UserSignupRequest
 from src.social.user.infra.router.user_signup_response import UserSignupResponse
 
@@ -8,4 +10,10 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/signup/{user_id}", status_code=status.HTTP_201_CREATED)
 async def signup_user(user_id: str, request: UserSignupRequest) -> UserSignupResponse:
-    raise NotImplementedError
+    user_signup = UserSignup(UserRepository())
+
+    await user_signup(
+        id_=user_id, name=request.name, username=request.username, email=request.email
+    )
+
+    return UserSignupResponse()
