a5edc25ad83bd5d4d26140fca9ca0cdaf0a2e58c
dimanu-py <diegomtz126@gmail.com>
2025-02-03 11:24:10 +0100

    refactor(user): use PostgresUserRepository inside router instead of in memory

--- src/social/user/infra/router/signup_user_router.py
+++ src/social/user/infra/router/signup_user_router.py
@@ -2,9 +2,14 @@ from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
 from src.shared.infra.HttpResponse import HttpResponse
+
+from src.shared.infra.persistence.sqlalchemy.session_maker import SessionMaker
+from src.shared.infra.settings import Settings
 from src.social.user.application.signup.user_signup import UserSignup
 from src.social.user.application.signup.user_signup_command import UserSignupCommand
-from src.social.user.infra.in_memory_user_repository import InMemoryUserRepository
+from src.social.user.infra.persistence.postgres_user_repository import (
+    PostgresUserRepository,
+)
 from src.social.user.infra.router.user_sign_up_request import UserSignupRequest
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -18,7 +23,9 @@ async def signup_user(user_id: str, request: UserSignupRequest) -> JSONResponse:
         username=request.username,
         email=request.email,
     )
-    user_signup = UserSignup(InMemoryUserRepository())
+    session_maker = SessionMaker(Settings())
+    repository = PostgresUserRepository(session_maker=session_maker)
+    user_signup = UserSignup(repository)
 
     await user_signup(command)
 
