bc9fbcfc1eb2f86be98d57a3ecc1e50f1ebe3306
dimanu-py <diegomtz126@gmail.com>
2025-02-04 11:23:42 +0100

    feat(shared): create HttpResponse and StatusCode classes to model api responses

---
+++ src/shared/infra/HttpResponse.py
@@ -0,0 +1,25 @@
+from fastapi.responses import JSONResponse
+
+from src.shared.domain.exceptions.domain_error import DomainError
+from src.shared.infra.status_code import StatusCode
+
+
+class HttpResponse:
+    @staticmethod
+    def domain_error(error: DomainError, status_code: StatusCode) -> JSONResponse:
+        return JSONResponse(content={"error": error.to_dict()}, status_code=status_code)
+
+    @staticmethod
+    def internal_error() -> JSONResponse:
+        return JSONResponse(
+            content={"error": "Internal server error"},
+            status_code=StatusCode.INTERNAL_SERVER_ERROR,
+        )
+
+    @staticmethod
+    def created() -> JSONResponse:
+        return JSONResponse(content={}, status_code=StatusCode.CREATED)
+
+    @staticmethod
+    def ok(content: dict) -> JSONResponse:
+        return JSONResponse(content=content, status_code=StatusCode.OK)
