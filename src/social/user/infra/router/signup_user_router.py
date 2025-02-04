2db3e7b6eddad9cf220a380276cf30a439ffca1b
dimanu-py <diegomtz126@gmail.com>
2025-02-04 11:30:22 +0100

    refactor(user): user new HttpResponse model and remove unused UserSignupResponse

--- src/social/user/infra/router/signup_user_router.py
+++ src/social/user/infra/router/signup_user_router.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+from src.shared.infra.HttpResponse import HttpResponse
 from src.social.user.application.signup.user_signup import UserSignup
 from src.social.user.application.signup.user_signup_command import UserSignupCommand
-
 from src.social.user.infra.in_memory_user_repository import InMemoryUserRepository
 from src.social.user.infra.router.user_sign_up_request import UserSignupRequest
-from src.social.user.infra.router.user_signup_response import UserSignupResponse
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/signup/{user_id}", status_code=status.HTTP_201_CREATED)
-async def signup_user(user_id: str, request: UserSignupRequest) -> UserSignupResponse:
+async def signup_user(user_id: str, request: UserSignupRequest) -> JSONResponse:
     command = UserSignupCommand(
         id=user_id,
         name=request.name,
@@ -21,4 +22,4 @@ async def signup_user(user_id: str, request: UserSignupRequest) -> UserSignupRes
 
     await user_signup(command)
 
-    return UserSignupResponse()
+    return HttpResponse.created()
