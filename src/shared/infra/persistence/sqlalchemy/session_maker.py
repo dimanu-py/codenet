7e9c4318c551255fa0f1e545d5141e41407efa34
dimanu-py <diegomtz126@gmail.com>
2025-02-06 10:32:08 +0100

    refactor(shared): create async engines and sessions with SQLAlchemy instead of working with synchronous operations

--- src/shared/infra/persistence/sqlalchemy/session_maker.py
+++ src/shared/infra/persistence/sqlalchemy/session_maker.py
@@ -1,23 +1,27 @@
-from sqlalchemy import create_engine, Engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.asyncio import (
+    async_sessionmaker,
+    create_async_engine,
+    AsyncEngine,
+    AsyncSession,
+)
 
 from src.shared.infra.persistence.sqlalchemy.base import (
     Base,
 )
-
 from src.shared.infra.settings import Settings
 
 
 class SessionMaker:
-    _session_maker: sessionmaker[Session]
-    _engine: Engine
+    _session_maker: async_sessionmaker[AsyncSession]
+    _engine: AsyncEngine
 
     def __init__(self, settings: Settings) -> None:
-        self._engine = create_engine(settings.postgres_url)
-        self._session_maker = sessionmaker(bind=self._engine)
+        self._engine = create_async_engine(settings.postgres_url)
+        self._session_maker = async_sessionmaker(bind=self._engine)
 
-    def get_session(self) -> Session:
+    def get_session(self) -> AsyncSession:
         return self._session_maker()
 
-    def create_tables(self) -> None:
-        Base.metadata.create_all(self._engine)
+    async def create_tables(self) -> None:
+        async with self._engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
