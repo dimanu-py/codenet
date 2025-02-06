2a224febdc281ba8d61999c33265708f93985aae
dimanu-py <diegomtz126@gmail.com>
2025-02-06 10:24:56 +0100

    feat(user): add id property to User to avoid direct accessing its attributes

--- src/social/user/domain/user.py
+++ src/social/user/domain/user.py
@@ -21,6 +21,10 @@ class User:
     def __eq__(self, other_user: "User") -> bool:
         return self.id_ == other_user.id_
 
+    @property
+    def id(self) -> UserId:
+        return self.id_
+
     @classmethod
     def signup(cls, id_: str, name: str, username: str, email: str) -> "User":
         return User(
