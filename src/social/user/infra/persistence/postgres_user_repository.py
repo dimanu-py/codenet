f0d1e75deb8068c2d82838f46dfd1dfa7a906157
dimanu-py <diegomtz126@gmail.com>
2025-02-10 21:11:47 +0100

    refactor(user): remove the use of SessionMaker and pass directly an AsyncEngine
    This change is made to be able to test better the class

--- src/social/user/infra/persistence/postgres_user_repository.py
+++ src/social/user/infra/persistence/postgres_user_repository.py
@@ -1,6 +1,7 @@
 from typing import override
 
-from src.shared.infra.persistence.sqlalchemy.session_maker import SessionMaker
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
+
 from src.social.user.domain.user import User
 from src.social.user.domain.user_id import UserId
 from src.social.user.domain.user_repository import UserRepository
@@ -8,20 +9,21 @@ from src.social.user.infra.persistence.user_model import UserModel
 
 
 class PostgresUserRepository(UserRepository):
-    _session_maker: SessionMaker
+    _engine: AsyncEngine
 
-    def __init__(self, session_maker: SessionMaker) -> None:
-        self._session_maker = session_maker
+    def __init__(self, engine: AsyncEngine) -> None:
+        self._engine = engine
+        self._session_maker = async_sessionmaker(bind=self._engine)
 
     @override
     async def save(self, user: User) -> None:
-        async with self._session_maker.get_session() as session:
+        async with self._session_maker() as session:
             user_to_save = UserModel(**user.to_dict())
             session.add(user_to_save)
             await session.commit()
 
     @override
     async def search(self, user_id: UserId) -> User:
-        async with self._session_maker.get_session() as session:
+        async with self._session_maker() as session:
             user = await session.get(UserModel, user_id.value)
             return user.to_aggregate() if user else None
