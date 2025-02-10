d8c6cf9f03196c8516f7b6f04217c4872b346a8a
dimanu-py <diegomtz126@gmail.com>
2025-02-10 21:16:16 +0100

    refactor(user): extract async engine generation to function and injected into router

--- src/social/user/infra/router/signup_user_router.py
+++ src/social/user/infra/router/signup_user_router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 from src.shared.infra.HttpResponse import HttpResponse
-
-from src.shared.infra.persistence.sqlalchemy.session_maker import SessionMaker
+from src.shared.infra.persistence.sqlalchemy.base import Base
 from src.shared.infra.settings import Settings
 from src.social.user.application.signup.user_signup import UserSignup
 from src.social.user.application.signup.user_signup_command import UserSignupCommand
@@ -15,16 +15,30 @@ from src.social.user.infra.router.user_sign_up_request import UserSignupRequest
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
+async def engine_generator() -> AsyncEngine:
+    engine = create_async_engine(Settings().postgres_url)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield engine
+
+    await engine.dispose()
+
+
 @router.post("/signup/{user_id}", status_code=status.HTTP_201_CREATED)
-async def signup_user(user_id: str, request: UserSignupRequest) -> JSONResponse:
+async def signup_user(
+    user_id: str,
+    request: UserSignupRequest,
+    engine: AsyncEngine = Depends(engine_generator),
+) -> JSONResponse:
     command = UserSignupCommand(
         id=user_id,
         name=request.name,
         username=request.username,
         email=request.email,
     )
-    session_maker = SessionMaker(Settings())  # type: ignore
-    repository = PostgresUserRepository(session_maker=session_maker)
+    repository = PostgresUserRepository(engine=engine)
     user_signup = UserSignup(repository)
 
     await user_signup(command)
