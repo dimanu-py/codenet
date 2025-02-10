5bc34fe7834ba28293a172d830adab0e0ae544d9
dimanu-py <diegomtz126@gmail.com>
2025-02-10 21:13:05 +0100

    refactor(user): create async fixture to yield an engine and be able to create and delete tables on every test

--- tests/social/user/infra/persistence/test_postgres_user_repository.py
+++ tests/social/user/infra/persistence/test_postgres_user_repository.py
@@ -1,7 +1,7 @@
 import pytest
 from expects import expect, equal
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
-from src.shared.infra.persistence.sqlalchemy.session_maker import SessionMaker
 from src.shared.infra.settings import Settings
 from src.social.user.infra.persistence.postgres_user_repository import (
     PostgresUserRepository,
@@ -10,22 +10,26 @@ from src.social.user.infra.persistence.user_model import UserModel
 from tests.social.user.domain.user_mother import UserMother
 
 
+@pytest.fixture
+async def engine() -> AsyncEngine:
+    settings = Settings()
+    engine = create_async_engine(settings.postgres_url)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(UserModel.metadata.create_all)
+
+    yield engine
+
+    async with engine.begin() as conn:
+        await conn.run_sync(UserModel.metadata.drop_all)
+    await engine.dispose()
+
+
 @pytest.mark.integration
 class TestPostgresUserRepository:
-    @classmethod
-    def setup_class(cls) -> None:
-        cls.session_maker = SessionMaker(settings=Settings())  # type: ignore
-        cls.session_maker.create_tables()
-
-    @classmethod
-    def teardown_class(cls) -> None:
-        with cls.session_maker.get_session() as session:
-            session.query(UserModel).delete()
-            session.commit()
-
     @pytest.mark.asyncio
-    async def test_should_save_user(self) -> None:
-        repository = PostgresUserRepository(session_maker=self.session_maker)
+    async def test_should_save_user(self, engine: AsyncEngine) -> None:
+        repository = PostgresUserRepository(engine)
         user = UserMother.any()
 
         await repository.save(user)
