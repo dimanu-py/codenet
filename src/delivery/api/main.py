8cd71bd82a5d5487c46815ec497c8c435d1d2205
dimanu-py <diegomtz126@gmail.com>
2025-02-18 09:33:08 +0100

    feat(api): implement lifespan to manage migration event on startup

--- src/delivery/api/main.py
+++ src/delivery/api/main.py
@@ -1,10 +1,22 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+from src.delivery.api.alembic_migrator import AlembicMigrator
 from src.shared.infra.http.http_response import HttpResponse
 from src.social.user.infra.router import signup_user_router as signup_user
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncGenerator[None, None]:
+    migrator = AlembicMigrator()
+    await migrator.migrate()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(signup_user.router)
 
