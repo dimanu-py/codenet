2e4b3a330d6c80e66f01943105db9c9d2b22d9bb
dimanu-py <diegomtz126@gmail.com>
2025-02-19 12:34:09 +0100

    feat(user): design interaction between router and use case

--- src/social/user/infra/router/search_user_router.py
+++ src/social/user/infra/router/search_user_router.py
@@ -1,10 +1,19 @@
+import json
 from collections.abc import AsyncGenerator
 
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
+from src.shared.domain.exceptions.domain_error import DomainError
+from src.shared.infra.http.http_response import HttpResponse
+from src.shared.infra.http.status_code import StatusCode
 from src.shared.infra.settings import Settings
+from src.social.user.application.search.search_user_query import SearchUserQuery
+from src.social.user.application.search.user_searcher import UserSearcher
+from src.social.user.infra.persistence.postgres_user_repository import (
+    PostgresUserRepository,
+)
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -23,4 +32,13 @@ async def get_user_by_criteria(
     filters: str,
     engine: AsyncEngine = Depends(engine_generator),
 ) -> JSONResponse:
-    raise NotImplementedError
+    query = SearchUserQuery(filters=json.loads(filters))
+    repository = PostgresUserRepository(engine=engine)
+    user_searcher = UserSearcher(repository=repository)
+
+    try:
+        user = await user_searcher(query)
+    except DomainError as error:
+        return HttpResponse.domain_error(error, status_code=StatusCode.BAD_REQUEST)
+
+    return HttpResponse.ok(user.to_dict() if user else {})
