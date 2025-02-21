a463221bf100c73d6e02683f34b280b0b94cfec8
dimanu <d.martinez@trevenque.es>
2025-02-21 18:43:52 +0100

    test(user): implement matching method in MockUserRepository and implement verification

--- tests/social/user/infra/persistence/mock_user_repository.py
+++ tests/social/user/infra/persistence/mock_user_repository.py
@@ -2,6 +2,7 @@ from unittest.mock import AsyncMock
 
 from expects import expect, equal
 
+from src.shared.domain.criteria.criteria import Criteria
 from src.social.user.domain.user import User
 from src.social.user.domain.user_id import UserId
 from src.social.user.domain.user_repository import UserRepository
@@ -10,6 +11,7 @@ from src.social.user.domain.user_repository import UserRepository
 class MockUserRepository(UserRepository):
     def __init__(self) -> None:
         self._mock_save = AsyncMock()
+        self._mock_match = AsyncMock()
 
     async def save(self, user: User) -> None:
         self._mock_save(user)
@@ -17,8 +19,18 @@ class MockUserRepository(UserRepository):
     async def search(self, user_id: UserId) -> User | None:
         raise NotImplementedError
 
+    async def matching(self, criteria: Criteria) -> list[User] | None:
+        return self._mock_match(criteria)  # type: ignore
+
     def should_save(self, user: User) -> None:
         def verify(expected_user: User) -> None:
             expect(user).to(equal(expected_user))
 
         self._mock_save = verify  # type: ignore
+
+    def should_match(self, criteria: Criteria, users: list[User]) -> None:
+        def verify(expected_criteria: Criteria) -> list[User]:
+            expect(criteria).to(equal(expected_criteria))
+            return users
+
+        self._mock_match = verify  # type: ignore
