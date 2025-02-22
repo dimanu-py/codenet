a49c7345c6c92339e0feffb84f8f3b384cf80d08
dimanu <d.martinez@trevenque.es>
2025-02-22 11:52:18 +0100

    test(user): create red integration test to verify criteria search in UserRepository

--- tests/social/shared/domain/criteria/criteria_mother.py
+++ tests/social/shared/domain/criteria/criteria_mother.py
@@ -6,3 +6,9 @@ class CriteriaMother:
     @classmethod
     def any(cls) -> Criteria:
         return Criteria(filters=FiltersMother.any())
+
+    @classmethod
+    def with_one_filter(cls, field: str, operator: str, value: str) -> Criteria:
+        return Criteria.from_primitives(
+            [{"field": field, "operator": operator, "value": value}]
+        )
--- tests/social/user/infra/persistence/test_postgres_user_repository.py
+++ tests/social/user/infra/persistence/test_postgres_user_repository.py
@@ -9,6 +9,7 @@ from src.social.user.infra.persistence.postgres_user_repository import (
     PostgresUserRepository,
 )
 from src.social.user.infra.persistence.user_model import UserModel
+from tests.social.shared.domain.criteria.criteria_mother import CriteriaMother
 from tests.social.user.domain.user_mother import UserMother
 
 
@@ -45,3 +46,18 @@ class TestPostgresUserRepository:
         saved_user = await repository.search(user.id)
 
         expect(user).to(equal(saved_user))
+
+    @pytest.mark.asyncio
+    async def test_should_match_a_user_based_on_criteria(
+        self, engine: AsyncEngine
+    ) -> None:
+        repository = PostgresUserRepository(engine)
+        user = UserMother.any()
+        criteria = CriteriaMother.with_one_filter(
+            field="username", operator="eq", value=user.username.value
+        )
+        await repository.save(user)
+
+        searched_users = await repository.matching(criteria)
+
+        expect(searched_users).to(equal([user]))
