5c54a83d95fbbc759d9309aeb5d4126ca231472e
dimanu <d.martinez@trevenque.es>
2025-02-22 12:55:57 +0100

    test(shared): create red test to verify equality filter gets generated with criteria converter

--- tests/social/shared/infra/criteria/test_criteria_to_sql_alchemy_converter.py
+++ tests/social/shared/infra/criteria/test_criteria_to_sql_alchemy_converter.py
@@ -1,11 +1,13 @@
 import pytest
 from expects import expect, equal
 
+from src.shared.domain.criteria.filter_operator import FilterOperator
 from src.shared.infra.criteria.criteria_to_sql_alchemy_converter import (
     CriteriaToSqlAlchemyConverter,
 )
 from src.social.user.infra.persistence.user_model import UserModel
 from tests.social.shared.domain.criteria.criteria_mother import CriteriaMother
+from tests.social.user.domain.user_name_mother import UserNameMother
 
 
 @pytest.mark.integration
@@ -17,5 +19,24 @@ class TestCriteriaToSqlAlchemyConverter:
         query = converter.convert(model=UserModel, criteria=criteria)
 
         expect(query).to(
-            equal("SELECT users.id, users.name, users.username, users.email \nFROM users")
+            equal(
+                "SELECT users.id, users.name, users.username, users.email \nFROM users"
+            )
+        )
+
+    def test_should_generate_select_query_with_one_filter(self) -> None:
+        user_name = UserNameMother.any()
+        criteria = CriteriaMother.with_one_filter(
+            "name", FilterOperator.EQUAL, user_name.value
+        )
+        converter = CriteriaToSqlAlchemyConverter()
+
+        query = converter.convert(model=UserModel, criteria=criteria)
+
+        expect(query).to(
+            equal(
+                f"SELECT users.id, users.name, users.username, users.email \n"
+                f"FROM users \n"
+                f"WHERE users.name = '{user_name.value}'"
+            )
         )
