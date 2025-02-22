b9da48816a040ed5dbfcc3b1ffa1bae842aba735
dimanu <d.martinez@trevenque.es>
2025-02-22 12:27:20 +0100

    feat(shared): implement basic select query with no filters

--- src/shared/infra/criteria/criteria_to_sql_alchemy_converter.py
+++ src/shared/infra/criteria/criteria_to_sql_alchemy_converter.py
@@ -1,7 +1,11 @@
+from sqlalchemy.sql import select
+
 from src.shared.domain.criteria.criteria import Criteria
 from src.shared.infra.persistence.sqlalchemy.base import Base
 
 
 class CriteriaToSqlAlchemyConverter:
-    def convert(self, model: type[Base], criteria: Criteria) -> str:
-        raise NotImplementedError
+    @staticmethod
+    def convert(model: type[Base], criteria: Criteria) -> str:
+        query = select(model)
+        return query.compile(compile_kwargs={"literal_binds": True}).string
