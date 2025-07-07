f0d3c3b73e568fd757b5ee66d5f0f69f9d205912
dimanu-py <diegomtz126@gmail.com>
2025-07-07 10:29:56 +0200

    test(user): add test to verify user that does not exist can't be removed

--- tests/social/user/application/removal/test_user_remover.py
+++ tests/social/user/application/removal/test_user_remover.py
@@ -1,10 +1,12 @@
 from src.social.user.application.removal.user_remover import UserRemover
+from tests.social.shared.expects.matchers import async_expect, raise_error
 from tests.social.user.application.removal.user_removal_command_mother import (
     UserRemovalCommandMother,
 )
 from tests.social.user.application.user_module_unit_test_config import (
     UserModuleUnitTestConfig,
 )
+from tests.social.user.domain.user_id_mother import UserIdMother
 from tests.social.user.domain.user_mother import UserMother
 
 
@@ -20,3 +22,13 @@ class TestUserRemoval(UserModuleUnitTestConfig):
         self._should_remove(user)
 
         await self._user_remover(command)
+
+    async def test_should_not_allow_to_remove_non_existing_user(self) -> None:
+        user_id = UserIdMother.any()
+        command = UserRemovalCommandMother.create(user_id=user_id.value)
+
+        self._should_not_find(user_id)
+
+        await async_expect(lambda: self._user_remover(command)).to(
+            raise_error(UserNotFoundError)
+        )
