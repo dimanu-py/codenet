50392820bdae75fc3728368dd3098703df1a0605
dimanu-py <diegomtz126@gmail.com>
2025-07-07 11:55:51 +0200

    feat(user): import implemented classes in removal user and return empty dict if successful

--- src/social/user/infra/api/removal/removal_user_router.py
+++ src/social/user/infra/api/removal/removal_user_router.py
@@ -8,6 +8,8 @@ from src.shared.domain.exceptions.domain_error import DomainError
 from src.shared.infra.http.http_response import HttpResponse
 from src.shared.infra.http.status_code import StatusCode
 from src.shared.infra.settings import Settings
+from src.social.user.application.removal.user_removal_command import UserRemovalCommand
+from src.social.user.application.removal.user_remover import UserRemover
 from src.social.user.infra.persistence.postgres_user_repository import (
     PostgresUserRepository,
 )
@@ -31,11 +33,11 @@ async def remove_user(
 ) -> JSONResponse:
     command = UserRemovalCommand(user_id=user_id)
     repository = PostgresUserRepository(engine=engine)
-    user_removal = UserRemoval(repository)
+    user_remover = UserRemover(repository)
 
     try:
-        await user_removal(command)
+        await user_remover(command)
     except DomainError as error:
         return HttpResponse.domain_error(error, status_code=StatusCode.BAD_REQUEST)
 
-    return HttpResponse.ok(content={"message": "User removed successfully"})
+    return HttpResponse.ok(content={})
