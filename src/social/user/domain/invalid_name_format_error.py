addd652762bb0afee15879ac752a746499066034
dimanu-py <diegomtz126@gmail.com>
2025-07-07 07:51:15 +0200

    refactor(user): modify exceptions to use new domain error implementation

--- src/social/user/domain/invalid_name_format_error.py
+++ src/social/user/domain/invalid_name_format_error.py
@@ -5,12 +5,4 @@ class InvalidNameFormatError(DomainError):
     def __init__(self) -> None:
         self._message = "Name cannot contain special characters or numbers."
         self._type = "invalid_name_format"
-        super().__init__(self._message)
-
-    @property
-    def type(self) -> str:
-        return self._type
-
-    @property
-    def message(self) -> str:
-        return self._message
+        super().__init__(self._message, self._type)
