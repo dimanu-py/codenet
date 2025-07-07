b11bf7038c86264ff7e3661f77ca2112b22ce81b
dimanu-py <diegomtz126@gmail.com>
2025-07-07 09:33:23 +0200

    feat(user): design how user remover use case deletes existing user

--- src/social/user/application/removal/user_remover.py
+++ src/social/user/application/removal/user_remover.py
@@ -1,14 +1,20 @@
 from src.social.user.application.removal.user_removal_command import UserRemovalCommand
-from src.social.user.infra.persistence.postgres_user_repository import (
-    PostgresUserRepository,
-)
+from src.social.user.domain.user import User
+from src.social.user.domain.user_id import UserId
+from src.social.user.domain.user_repository import UserRepository
 
 
 class UserRemover:
-    _repository: PostgresUserRepository
+    _repository: UserRepository
 
-    def __init__(self, repository: PostgresUserRepository) -> None:
+    def __init__(self, repository: UserRepository) -> None:
         self._repository = repository
 
     async def __call__(self, command: UserRemovalCommand) -> None:
-        raise NotImplementedError
+        existing_user = await self._ensure_user_to_remove_exists(command.user_id)
+        await self._repository.delete(existing_user)
+
+    async def _ensure_user_to_remove_exists(self, user_id: str) -> User:
+        user = await self._repository.find(UserId(user_id))
+        if not user:
+            raise UserNotFoundError(user_id)
