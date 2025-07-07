05b275c583165ea8d5902a07b62c6ce4384ac8c1
dimanu-py <diegomtz126@gmail.com>
2025-07-07 09:26:43 +0200

    test(user): create '_user_removal' attribute in unit test

--- tests/social/user/application/removal/test_user_removal.py
+++ tests/social/user/application/removal/test_user_removal.py
@@ -1,4 +1,6 @@
-from tests.social.user.application.removal.user_removal_command_mother import UserRemovalCommandMother
+from tests.social.user.application.removal.user_removal_command_mother import (
+    UserRemovalCommandMother,
+)
 from tests.social.user.application.user_module_unit_test_config import (
     UserModuleUnitTestConfig,
 )
@@ -6,6 +8,9 @@ from tests.social.user.domain.user_mother import UserMother
 
 
 class TestUserRemoval(UserModuleUnitTestConfig):
+    def setup_method(self) -> None:
+        self._user_removal = UserRemoval(repository=self._repository)
+
     async def test_should_remove_existing_user(self) -> None:
         user = UserMother.any()
         self._should_find(user)
