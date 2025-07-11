014eb47f2edb67af624b304b92f619d1c7af030f
dimanu-py <diegomtz126@gmail.com>
2025-07-11 23:14:19 +0200

    test(user): use UuidMother for user id mother

--- tests/social/user/domain/user_id_mother.py
+++ tests/social/user/domain/user_id_mother.py
@@ -1,8 +1,11 @@
 from src.social.user.domain.user_id import UserId
 from tests.shared.domain.random_generator import RandomGenerator
+from tests.shared.domain.value_objects.uuid_primitives_mother import (
+    UuidPrimitivesMother,
+)
 
 
 class UserIdMother:
     @staticmethod
     def any() -> UserId:
-        return UserId(RandomGenerator.uuid())
+        return UserId(UuidPrimitivesMother.any())
