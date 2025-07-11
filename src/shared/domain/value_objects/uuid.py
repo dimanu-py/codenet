cd9f01cc35944ccdbc154b8aa9e61e4883a5c4d3
dimanu-py <diegomtz126@gmail.com>
2025-07-11 23:07:37 +0200

    feat(shared): raise error when uuid value is none

--- src/shared/domain/value_objects/uuid.py
+++ src/shared/domain/value_objects/uuid.py
@@ -4,6 +4,7 @@ from src.shared.domain.exceptions.incorrect_value_type_error import (
     IncorrectValueTypeError,
 )
 from src.shared.domain.exceptions.invalid_id_format_error import InvalidIdFormatError
+from src.shared.domain.exceptions.required_value_error import RequiredValueError
 from src.shared.domain.value_objects.value_object import ValueObject
 
 
@@ -12,8 +13,10 @@ class Uuid(ValueObject[str]):
         super().__init__(value)
 
     def _validate(self, value: str) -> None:
+        if value is None:
+            raise RequiredValueError
         if not isinstance(value, str):
-            raise IncorrectValueTypeError
+            raise IncorrectValueTypeError(value)
         try:
             UUID(value)
         except ValueError:
