44bfe5e260aee186395f65ac0215638e30519cb4
dimanu-py <diegomtz126@gmail.com>
2025-07-12 09:54:12 +0200

    refactor: fix lint errors

--- tests/social/user/domain/user_id_mother.py
+++ tests/social/user/domain/user_id_mother.py
@@ -1,5 +1,4 @@
 from src.social.user.domain.user_id import UserId
-from tests.shared.domain.random_generator import RandomGenerator
 from tests.shared.domain.value_objects.uuid_primitives_mother import (
     UuidPrimitivesMother,
 )
