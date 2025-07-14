b614bd1e4c74cfc0f4bb800cc9f985f98767cf4c
dimanu-py <diegomtz126@gmail.com>
2025-07-14 15:21:43 +0200

    feat(shared): create new ApplicationError type that will represent errors raised directly by use cases

---
+++ src/shared/domain/exceptions/application_error.py
@@ -0,0 +1,7 @@
+from src.shared.domain.exceptions.base_error import BaseError
+
+
+class ApplicationError(BaseError):
+    """Errors produced by the application layer of the application."""
+
+    ...
