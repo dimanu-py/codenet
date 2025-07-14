c64b75b87213a27c7d302703651b8b76b523d04f
dimanu-py <diegomtz126@gmail.com>
2025-07-14 15:22:25 +0200

    feat(user): make UserNotFoundError of type ApplicationError instead of DomainError

--- src/social/user/application/removal/user_not_found_error.py
+++ src/social/user/application/removal/user_not_found_error.py
@@ -1,7 +1,7 @@
-from src.shared.domain.exceptions.domain_error import DomainError
+from src.shared.domain.exceptions.application_error import ApplicationError
 
 
-class UserNotFoundError(DomainError):
+class UserNotFoundError(ApplicationError):
     def __init__(self, user_id: str) -> None:
         self._message = f"User with id {user_id} not found"
         self._type = "user_not_found"
