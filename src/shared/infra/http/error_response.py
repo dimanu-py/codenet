da223192e44d58cbdfcdd32e9442da7b13971672
dimanu-py <diegomtz126@gmail.com>
2025-07-14 15:13:34 +0200

    feat(shared): create subtypes of error responses to specify easier status codes and details examples, as well as show possible errors on documentation

--- src/shared/infra/http/error_response.py
+++ src/shared/infra/http/error_response.py
@@ -1,8 +1,11 @@
+from abc import ABC
+
+from fastapi import status
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class ErrorResponse(BaseModel):
+class ErrorResponse(ABC, BaseModel):
     status_code: int
     detail: str
 
@@ -11,3 +14,18 @@ class ErrorResponse(BaseModel):
             content={"detail": self.detail},
             status_code=self.status_code,
         )
+
+
+class UnprocessableEntityError(ErrorResponse):
+    status_code: int = Field(default=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    detail: str = Field(default="Unprocessable Entity")
+
+
+class ResourceNotFoundError(ErrorResponse):
+    status_code: int = Field(default=status.HTTP_404_NOT_FOUND)
+    detail: str = Field(default="Not Found")
+
+
+class InternalServerError(ErrorResponse):
+    status_code: int = Field(default=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    detail: str = Field(default="An unexpected error occurred.")
