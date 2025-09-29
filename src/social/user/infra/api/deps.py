3e300b482d812433f6e06fb13cf61a92c50cda2b
dimanu-py <diegomtz126@gmail.com>
2025-09-29 10:39:29 +0200

    feat(user): create file to have common dependencies for user aggregate endpoints

---
+++ src/social/user/infra/api/deps.py
@@ -0,0 +1,23 @@
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio.engine import AsyncEngine
+
+from src.shared.infra.settings import Settings
+from src.social.user.infra.persistence.postgres_user_repository import (
+    PostgresUserRepository,
+)
+
+
+async def engine_generator() -> AsyncGenerator[AsyncEngine]:
+    engine = create_async_engine(Settings().postgres_url)  # type: ignore
+
+    try:
+        yield engine
+    finally:
+        await engine.dispose()
+
+
+def postgres_user_repository(engine: AsyncEngine = Depends(engine_generator)) -> PostgresUserRepository:
+    return PostgresUserRepository(engine=engine)
