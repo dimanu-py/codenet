f632904180f707817eeecc3e1c8d5690cb1e1529
dimanu-py <diegomtz126@gmail.com>
2025-09-29 20:45:55 +0200

    feat(user): remove not needed engine_generator function and implement new function to be able to inject an async session to the postgres user repository

--- src/social/user/infra/api/deps.py
+++ src/social/user/infra/api/deps.py
@@ -2,24 +2,28 @@ from collections.abc import AsyncGenerator
 
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio.engine import AsyncEngine
+from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from src.shared.infra.settings import Settings
 from src.social.user.infra.persistence.postgres_user_repository import (
     PostgresUserRepository,
 )
 
+settings = Settings()  # type: ignore
+engine = create_async_engine(str(settings.postgres_url))
 
-async def engine_generator() -> AsyncGenerator[AsyncEngine]:
-    engine = create_async_engine(Settings().postgres_url)  # type: ignore
 
-    try:
-        yield engine
-    finally:
-        await engine.dispose()
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSession(engine) as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
 
 
 def postgres_user_repository(
-    engine: AsyncEngine = Depends(engine_generator),
+    session: AsyncSession = Depends(get_async_session),
 ) -> PostgresUserRepository:
-    return PostgresUserRepository()
+    return PostgresUserRepository(session)
