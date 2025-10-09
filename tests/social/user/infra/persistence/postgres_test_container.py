75385b050673171a1689c0414c2aab58648a28c8
dimanu-py <diegomtz126@gmail.com>
2025-10-09 10:58:52 +0200

    fix(user): modify postgres test container start to wait for healthcheck success report

--- tests/social/user/infra/persistence/postgres_test_container.py
+++ tests/social/user/infra/persistence/postgres_test_container.py
@@ -1,7 +1,7 @@
 from typing import Self
 
 from testcontainers.core.container import DockerContainer
-from testcontainers.core.wait_strategies import LogMessageWaitStrategy
+from testcontainers.core.wait_strategies import HealthcheckWaitStrategy
 
 
 class PostgresTestContainer(DockerContainer):
@@ -10,6 +10,11 @@ class PostgresTestContainer(DockerContainer):
     _USER = "test_user"
     _PASSWORD = "test_password"
     _DATABASE_NAME = "test_db"
+    _HEALTHCHECK_INTERVAL_SECONDS = 1
+    _HEALTHCHECK_TIMEOUT_SECONDS = 3
+    _HEALTHCHECK_RETRIES_LIMIT = 5
+    _HEALTHCHECK_START_PERIOD_SECONDS = 5
+    _TO_NANOSECONDS = 1_000_000_000
 
     def __init__(self) -> None:
         super().__init__(self._POSTGRES_IMAGE)
@@ -17,10 +22,20 @@ class PostgresTestContainer(DockerContainer):
         self.with_env("POSTGRES_DB", self._DATABASE_NAME)
         self.with_env("POSTGRES_USER", self._USER)
         self.with_env("POSTGRES_PASSWORD", self._PASSWORD)
-        self.waiting_for(LogMessageWaitStrategy("database system is ready to accept connections"))
+        self.with_kwargs(
+            healthcheck={
+                "test": ["CMD-SHELL", f"pg_isready -U {self._USER} -d {self._DATABASE_NAME}"],
+                "interval": self._HEALTHCHECK_INTERVAL_SECONDS * self._TO_NANOSECONDS,
+                "timeout": self._HEALTHCHECK_TIMEOUT_SECONDS * self._TO_NANOSECONDS,
+                "retries": self._HEALTHCHECK_RETRIES_LIMIT,
+                "start_period": self._HEALTHCHECK_START_PERIOD_SECONDS * self._TO_NANOSECONDS,
+            }
+        )
+        self.wait_strategy = HealthcheckWaitStrategy()
 
     def start(self) -> Self:
         super().start()
+        self.wait_strategy.wait_until_ready(self)
         return self
 
     def get_base_url(self) -> str:
