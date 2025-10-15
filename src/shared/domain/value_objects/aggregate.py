adf689f82bd3378f92232964313f3d000e91ed38
dimanu-py <diegomtz126@gmail.com>
2025-10-15 07:54:30 +0200

    refactor(shared): implement abstract constructor in Aggregate class

--- src/shared/domain/value_objects/aggregate.py
+++ src/shared/domain/value_objects/aggregate.py
@@ -1,5 +1,9 @@
+from abc import abstractmethod
+
 from sindripy.value_objects import Aggregate as AggregateRoot
 
 
 class Aggregate(AggregateRoot):
-    ...
+    @abstractmethod
+    def __init__(self) -> None:
+        super().__init__()
