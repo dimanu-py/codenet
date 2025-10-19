fab5555ad2e8b252f7315c3192242ad1d40dfc3d
dimanu-py <diegomtz126@gmail.com>
2025-10-19 11:44:39 +0200

    test(user): add validation tests for user signup to handle invalid name, username, and email formats

--- tests/social/user/infra/api/signup/test_signup_user_router.py
+++ tests/social/user/infra/api/signup/test_signup_user_router.py
@@ -6,6 +6,9 @@ from expects import equal, expect
 
 from src.shared.domain.exceptions.domain_error import DomainError
 from src.social.user.application.signup.user_signup import UserSignup
+from src.social.user.domain.user_email import InvalidEmailFormatError
+from src.social.user.domain.user_name import InvalidNameFormatError
+from src.social.user.domain.user_username import InvalidUsernameFormatError
 from src.social.user.infra.api.signup.signup_user_router import signup_user
 from src.social.user.infra.api.signup.user_sign_up_request import UserSignupRequest
 from tests.shared.expects.async_stub import AsyncStub
@@ -56,8 +59,61 @@ class TestSignupUserRouter:
         )
 
         expect(response.status_code).to(equal(422))
-        expect(json.loads(response.body)).to((
-            equal({
-                "detail": "User id must be a valid UUID"
-            })
-        ))
+        expect(json.loads(response.body)).to((equal({"detail": "User id must be a valid UUID"})))
+
+    async def test_should_return_422_when_user_name_has_invalid_character(self) -> None:
+        request_body = UserSignupRequest(
+            name="Invalid@Name",
+            username=UserUsernameMother.any().value,
+            email=UserEmailMother.any().value,
+        )
+        user_id = UserIdMother.any().value
+        user_signup = AsyncStub(UserSignup)
+        when(user_signup).execute(ANY_ARG).raises(InvalidNameFormatError)
+
+        response = await signup_user(
+            request=request_body,
+            user_id=user_id,
+            user_signup=user_signup,
+        )
+
+        expect(response.status_code).to(equal(422))
+        expect(json.loads(response.body)).to((equal({"detail": "Name cannot contain special characters or numbers."})))
+
+    async def test_should_return_422_when_user_username_has_invalid_character(self) -> None:
+        request_body = UserSignupRequest(
+            name=UserNameMother.any().value,
+            username="Invalid*Username",
+            email=UserEmailMother.any().value,
+        )
+        user_id = UserIdMother.any().value
+        user_signup = AsyncStub(UserSignup)
+        when(user_signup).execute(ANY_ARG).raises(InvalidUsernameFormatError)
+
+        response = await signup_user(
+            request=request_body,
+            user_id=user_id,
+            user_signup=user_signup,
+        )
+
+        expect(response.status_code).to(equal(422))
+        expect(json.loads(response.body)).to((equal({"detail": "Username cannot contain special characters"})))
+
+    async def test_should_return_422_when_email_does_not_have_valid_format(self) -> None:
+        request_body = UserSignupRequest(
+            name=UserNameMother.any().value,
+            username=UserUsernameMother.any().value,
+            email="invalid-email-format",
+        )
+        user_id = UserIdMother.any().value
+        user_signup = AsyncStub(UserSignup)
+        when(user_signup).execute(ANY_ARG).raises(InvalidEmailFormatError)
+
+        response = await signup_user(
+            request=request_body,
+            user_id=user_id,
+            user_signup=user_signup,
+        )
+
+        expect(response.status_code).to(equal(422))
+        expect(json.loads(response.body)).to((equal({"detail": "Email cannot contain special characters and must contain '@' and '.'"})))
