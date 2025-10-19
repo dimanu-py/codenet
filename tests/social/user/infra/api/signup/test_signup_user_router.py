8b59fc2596e2b9929b495eef40372538de640b37
dimanu-py <diegomtz126@gmail.com>
2025-10-19 11:46:05 +0200

    refactor(user): rename test for valid signup data and remove unused test for invalid user ID format

--- tests/social/user/infra/api/signup/test_signup_user_router.py
+++ tests/social/user/infra/api/signup/test_signup_user_router.py
@@ -4,7 +4,6 @@ import pytest
 from doublex import ANY_ARG, when
 from expects import equal, expect
 
-from src.shared.domain.exceptions.domain_error import DomainError
 from src.social.user.application.signup.user_signup import UserSignup
 from src.social.user.domain.user_email import InvalidEmailFormatError
 from src.social.user.domain.user_name import InvalidNameFormatError
@@ -21,7 +20,7 @@ from tests.social.user.domain.mothers.user_username_mother import UserUsernameMo
 @pytest.mark.unit
 @pytest.mark.asyncio
 class TestSignupUserRouter:
-    async def test_should_receive_successful_response_when_sending_valid_signup_data(self) -> None:
+    async def test_should_return_201_when_signup_data_is_valid(self) -> None:
         request_body = UserSignupRequest(
             name=UserNameMother.any().value,
             username=UserUsernameMother.any().value,
@@ -39,27 +38,6 @@ class TestSignupUserRouter:
 
         expect(response.status_code).to(equal(201))
         expect(json.loads(response.body)).to(equal({"resource": f"/app/users/{user_id}"}))
-
-    async def test_should_return_422_when_user_id_does_not_fulfill_uuid_format(self) -> None:
-        request_body = UserSignupRequest(
-            name=UserNameMother.any().value,
-            username=UserUsernameMother.any().value,
-            email=UserEmailMother.any().value,
-        )
-        invalid_user_id = "invalid-uuid-format"
-        user_signup = AsyncStub(UserSignup)
-        when(user_signup).execute(ANY_ARG).raises(
-            DomainError(message="User id must be a valid UUID", error_type="invalid_user_id")
-        )
-
-        response = await signup_user(
-            request=request_body,
-            user_id=invalid_user_id,
-            user_signup=user_signup,
-        )
-
-        expect(response.status_code).to(equal(422))
-        expect(json.loads(response.body)).to((equal({"detail": "User id must be a valid UUID"})))
 
     async def test_should_return_422_when_user_name_has_invalid_character(self) -> None:
         request_body = UserSignupRequest(
