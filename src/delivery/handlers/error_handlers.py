3beb4f0acd3e420c6c479c6c66b755c4579e50ff
dimanu-py <diegomtz126@gmail.com>
2025-10-20 16:22:27 +0200

    refactor(delivery): remove unused exception handlers and simplify error handling

--- src/delivery/handlers/error_handlers.py
+++ src/delivery/handlers/error_handlers.py
@@ -1,78 +1,13 @@
 from fastapi import Request
-from fastapi.exception_handlers import request_validation_exception_handler
-from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
-from src.shared.domain.exceptions.application_error import ApplicationError
-from src.shared.domain.exceptions.domain_error import DomainError
 from src.shared.infra.http.error_response import (
     InternalServerError,
-    ResourceNotFoundError,
-    UnprocessableEntityError,
 )
-from src.shared.infra.logger.fastapi_file_logger import create_api_logger
-
-logger = create_api_logger(name="codenet")
 
 
 async def unexpected_exception_handler(
-    request: Request,
-    exc: Exception,
+    _: Request,
+    __: Exception,
 ) -> JSONResponse:
-    logger.error(
-        message=f"error - {request.url.path}",
-        details={
-            "error": {
-                "message": str(exc),
-                "type": "unexpected_error",
-            },
-            "method": request.method,
-            "source": request.url.path,
-        },
-    )
     return InternalServerError().as_json()
-
-
-async def domain_error_handler(
-    request: Request,
-    exc: DomainError,
-) -> JSONResponse:
-    logger.error(
-        message=f"error - {request.url.path}",
-        details={
-            "error": exc.to_primitives(),
-            "method": request.method,
-            "source": request.url.path,
-        },
-    )
-    return UnprocessableEntityError(detail=exc.message).as_json()
-
-
-async def application_error_handler(
-    request: Request,
-    exc: ApplicationError,
-) -> JSONResponse:
-    logger.error(
-        message=f"error - {request.url.path}",
-        details={
-            "error": exc.to_primitives(),
-            "method": request.method,
-            "source": request.url.path,
-        },
-    )
-    return ResourceNotFoundError(detail=exc.message).as_json()
-
-
-async def validation_error_handler(
-    request: Request,
-    exc: RequestValidationError,
-) -> JSONResponse:
-    logger.error(
-        message=f"error - {request.url.path}",
-        details={
-            "error": {"message": str(exc), "type": "validation_error"},
-            "method": request.method,
-            "source": request.url.path,
-        },
-    )
-    return await request_validation_exception_handler(request, exc)
--- src/delivery/main.py
+++ src/delivery/main.py
@@ -3,18 +3,12 @@ from contextlib import asynccontextmanager
 
 from asgi_correlation_id import CorrelationIdMiddleware
 from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
 
 from src.delivery.alembic_migrator import AlembicMigrator
 from src.delivery.handlers.error_handlers import (
-    application_error_handler,
-    domain_error_handler,
     unexpected_exception_handler,
-    validation_error_handler,
 )
 from src.delivery.middleware.fast_api_log_middleware import FastapiLogMiddleware
-from src.shared.domain.exceptions.application_error import ApplicationError
-from src.shared.domain.exceptions.domain_error import DomainError
 from src.shared.infra.logger.fastapi_file_logger import (
     create_api_logger,
 )
@@ -35,7 +29,4 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(FastapiLogMiddleware, logger=logger)  # type: ignore
 app.add_middleware(CorrelationIdMiddleware)  # type: ignore
 app.add_exception_handler(Exception, unexpected_exception_handler)
-app.add_exception_handler(DomainError, domain_error_handler)  # type: ignore
-app.add_exception_handler(ApplicationError, application_error_handler)  # type: ignore
-app.add_exception_handler(RequestValidationError, validation_error_handler)  # type: ignore
 app.include_router(user_routes.routes)
