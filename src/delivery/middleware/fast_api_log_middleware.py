a044f25b755654912175ba0a081034d302ccc092
dimanu-py <diegomtz126@gmail.com>
2025-10-20 18:16:18 +0200

    feat(delivery): enhance logging with structured success and error details

--- src/delivery/middleware/fast_api_log_middleware.py
+++ src/delivery/middleware/fast_api_log_middleware.py
@@ -3,7 +3,7 @@ import time
 from fastapi import FastAPI, Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
-from src.shared.infra.logger.fastapi_file_logger import FastApiFileLogger
+from src.shared.infra.logger.fastapi_file_logger import FastApiFileLogger, SuccessRecordDetails, ErrorRecordDetails
 
 
 class FastapiLogMiddleware(BaseHTTPMiddleware):
@@ -17,14 +17,24 @@ class FastapiLogMiddleware(BaseHTTPMiddleware):
         process_time = time.perf_counter() - start_time
 
         if response.status_code < 400:
-            self._logger.info(
-                message=f"success - {request.url.path}",
-                details={
-                    "method": request.method,
-                    "source": request.url.path,
-                    "process_time": process_time,
-                    "status_code": response.status_code,
-                },
+            self._logger.success(
+                message=request.url.path,
+                details=SuccessRecordDetails(
+                    method=request.method,
+                    source=request.url.path,
+                    process_time=process_time,
+                    status_code=response.status_code,
+                ),
+            )
+        else:
+            self._logger.error(
+                message=request.url.path,
+                details=ErrorRecordDetails(
+                    method=request.method,
+                    error_message=str(response),
+                    source=request.url.path,
+                    status_code=response.status_code,
+                ),
             )
 
         return response
