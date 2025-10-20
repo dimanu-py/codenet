ea86755730bfcc19042161bf7dbffdf27b99e6cf
dimanu-py <diegomtz126@gmail.com>
2025-10-20 08:33:25 +0200

    test(user): refactor search user router tests for improved clarity and contract verification

--- tests/social/user/infra/api/search/test_search_user_router.py
+++ tests/social/user/infra/api/search/test_search_user_router.py
@@ -5,6 +5,7 @@ from doublex import when, ANY_ARG
 from expects import expect, equal
 
 from src.social.user.application.search.user_searcher import UserSearcher
+from src.social.user.domain.user import User
 from src.social.user.infra.api.search.search_user_router import get_user_by_criteria
 from tests.shared.domain.criteria.mothers.criteria_mother import CriteriaMother
 from tests.shared.expects.async_stub import AsyncStub
@@ -14,35 +15,43 @@ from tests.social.user.domain.mothers.user_mother import UserMother
 @pytest.mark.unit
 @pytest.mark.asyncio
 class TestSearchUserRouter:
+    def setup_method(self) -> None:
+        self._user_searcher = AsyncStub(UserSearcher)
+        self._response = None
+
     async def test_should_return_200_when_users_are_found_and_response_contains_list_of_users(self) -> None:
         filters = CriteriaMother.any().to_primitives()
         user = UserMother.any()
-        user_searcher = AsyncStub(UserSearcher)
-        when(user_searcher).execute(
-            ANY_ARG,
-        ).returns([user])
+        self._should_find_user(user)
 
-        response = await get_user_by_criteria(
+        self._response = await get_user_by_criteria(
             filter=json.dumps(filters),
-            user_searcher=user_searcher,
+            user_searcher=self._user_searcher,
         )
 
-        found_users = {"users": [user.to_primitives()]}
-        expect(response.status_code).to(equal(200))
-        expect(json.loads(response.body)).to(equal(found_users))
+        self._assert_contract_is_met_with(200, [user.to_primitives()])
 
     async def test_should_return_200_when_users_are_not_found_and_response_is_an_empty_list(self) -> None:
         filters = CriteriaMother.any().to_primitives()
-        user_searcher = AsyncStub(UserSearcher)
-        when(user_searcher).execute(
+        self._should_not_find_user()
+
+        self._response = await get_user_by_criteria(
+            filter=json.dumps(filters),
+            user_searcher=self._user_searcher,
+        )
+
+        self._assert_contract_is_met_with(200, [])
+
+    def _should_not_find_user(self) -> None:
+        when(self._user_searcher).execute(
             ANY_ARG,
         ).returns([])
 
-        response = await get_user_by_criteria(
-            filter=json.dumps(filters),
-            user_searcher=user_searcher,
-        )
+    def _should_find_user(self, user: User) -> None:
+        when(self._user_searcher).execute(
+            ANY_ARG,
+        ).returns([user])
 
-        found_users = {"users": []}
-        expect(response.status_code).to(equal(200))
-        expect(json.loads(response.body)).to(equal(found_users))
+    def _assert_contract_is_met_with(self, expected_status_code: int, found_users: list) -> None:
+        expect(self._response.status_code).to(equal(expected_status_code))
+        expect(json.loads(self._response.body)).to(equal({"users": found_users}))
