2e97dab6102816119b64ce3949be8bfc1d3a1c94
dimanu-py <diegomtz126@gmail.com>
2026-01-28 10:38:04 +0100

    refactor(shared): raise NotImplementedError in abstract aggregate constructor

--- src/shared/domain/value_objects/aggregate.py
+++ src/shared/domain/value_objects/aggregate.py
@@ -6,4 +6,4 @@ from sindripy.value_objects import Aggregate as AggregateRoot
 class Aggregate(AggregateRoot):
     @abstractmethod
     def __init__(self) -> None:
-        super().__init__()
+        raise NotImplementedError
