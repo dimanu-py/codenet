91b489495cc3aef941a71375ada880a3b5f10cc5
dimanu-py <diegomtz126@gmail.com>
2026-01-28 21:47:34 +0100

    test(authenticate): rename test_authenticate_router to test_authenticate_controller and update class name

--- tests/social/auth/infra/api/authenticate/test_authenticate_controller.py
+++ tests/social/auth/infra/api/authenticate/test_authenticate_controller.py
@@ -3,5 +3,5 @@ import pytest
 
 @pytest.mark.acceptance
 @pytest.mark.asyncio
-class TestAuthenticateRouter:
+class TestAuthenticateController:
     async def test_should_generate_access_token_for_valid_user_credentials(self) -> None: ...
