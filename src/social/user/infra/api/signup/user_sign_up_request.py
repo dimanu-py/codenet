db0990b4ce1ff7d470f4ae7b3e753a67f9dfa95c
dimanu-py <diegomtz126@gmail.com>
2026-01-28 19:25:43 +0100

    feat(user): add password field to user sign up request model

--- src/social/user/infra/api/signup/user_sign_up_request.py
+++ src/social/user/infra/api/signup/user_sign_up_request.py
@@ -5,3 +5,4 @@ class UserSignupRequest(BaseModel):
     name: str = Field(examples=["John Doe"])
     username: str = Field(examples=["johndoe"])
     email: str = Field(examples=["johndoe@hotmail.com"])
+    password: str = Field(examples=["strongpassword123"])
