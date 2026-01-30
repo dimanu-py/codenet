8b4dc3899f14db195ec9f381216f543cc84b5ac1
dimanu-py <diegomtz126@gmail.com>
2026-01-30 17:10:15 +0100

    feat(user): prevent signup with existing username in UserSignup use case

--- src/social/user/application/signup/user_signup.py
+++ src/social/user/application/signup/user_signup.py
@@ -1,6 +1,8 @@
 from src.social.user.application.signup.user_signup_command import UserSignupCommand
 from src.social.user.domain.user import User
 from src.social.user.domain.user_repository import UserRepository
+from src.social.user.domain.user_username import UserUsername
+from tests.social.user.domain.user_already_exists import UsernameAlreadyExists
 
 
 class UserSignup:
@@ -10,6 +12,8 @@ class UserSignup:
         self._repository = repository
 
     async def execute(self, command: UserSignupCommand) -> None:
+        await self._ensure_user_with_same_username_is_not_signed_up(command.username)
+
         user = User(
             id=command.id,
             name=command.name,
@@ -22,3 +26,8 @@ class UserSignup:
 
     async def _store_user(self, user: User) -> None:
         await self._repository.save(user)
+
+    async def _ensure_user_with_same_username_is_not_signed_up(self, username: str) -> None:
+        already_signed_up_user = await self._repository.search(UserUsername(username))
+        if already_signed_up_user:
+            raise UsernameAlreadyExists
