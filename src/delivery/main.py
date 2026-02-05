78ddf0b41cd8f62bae4570daff2f0b4e7df77416
dimanu-py <diegomtz126@gmail.com>
2026-02-05 14:06:42 +0100

    refactor(delivery): update import path for user routes to improve module structure

--- src/delivery/main.py
+++ src/delivery/main.py
@@ -11,7 +11,7 @@ from src.delivery.handlers.error_handlers import (
     unexpected_exception_handler,
 )
 from src.delivery.middleware.fast_api_log_middleware import FastapiLogMiddleware
-from src.delivery.routers.social.user.routes import routes as user_routes
+from src.delivery.routers.social.user import routes as user_routes
 from src.shared.infra.logger.fastapi_file_logger import (
     create_api_logger,
 )
