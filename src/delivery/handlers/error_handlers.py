a4855e5d14060e83e1ae98f6c641852da2e6d972
dimanu-py <diegomtz126@gmail.com>
2026-02-05 13:49:20 +0100

    refactor(delivery): update sindri_validation_error_handler to accept a generic Exception and avoid mypy fail for liskov substitution principle
    The type of the passed argument has to be the same or more general that the one that is specified as type hint, otherwise it will fail

--- src/delivery/handlers/error_handlers.py
+++ src/delivery/handlers/error_handlers.py
@@ -1,6 +1,5 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from sindripy.value_objects import SindriValidationError
 
 from src.delivery.routers.fastapi_response import FastAPIResponse
 from src.shared.infra.http.error_response import (
@@ -18,7 +17,7 @@ async def unexpected_exception_handler(
 
 async def sindri_validation_error_handler(
     _: Request,
-    error: SindriValidationError,
+    error: Exception,
 ) -> JSONResponse:
     return FastAPIResponse.as_json(
         UnprocessableEntityError(error={"type": "validation_error", "message": error.message})
