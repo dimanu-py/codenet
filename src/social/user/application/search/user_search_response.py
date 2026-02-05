f2774dab6c30b87e7d3b148f9fd43ad8861a5edc
dimanu-py <diegomtz126@gmail.com>
2026-02-05 13:43:30 +0100

    refactor(user): remove unused variable for UserSearchResponse and delete dto that is not needed for now

--- src/social/user/application/search/user_search_response.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import asdict, dataclass
-
-
-@dataclass(frozen=True)
-class UserSearchResponse:
-    data: list[dict]
-
-    def dump(self) -> dict:
-        return asdict(self)
--- src/social/user/infra/api/search/user_search_controller.py
+++ src/social/user/infra/api/search/user_search_controller.py
@@ -1,6 +1,5 @@
 from src.shared.infra.http.success_response import OkResponse, SuccessResponse
 from src.social.user.application.search.search_user_query import SearchUserQuery
-from src.social.user.application.search.user_search_response import UserSearchResponse
 from src.social.user.application.search.user_searcher import UserSearcher
 
 
@@ -12,7 +11,5 @@ class UserSearchController:
         query = SearchUserQuery(filters=filters)
 
         users = await self._searcher.execute(query)
-
-        response = UserSearchResponse([user.to_public_primitives() for user in users])
 
         return OkResponse(data=[user.to_public_primitives() for user in users])
