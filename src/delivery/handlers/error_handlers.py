06d37a8d95f59f091429fce1697a50caddcacba8
dimanu-py <diegomtz126@gmail.com>
2026-02-05 08:51:06 +0100

    refactor(delivery): update error responses in error handlers to use FastAPIResponse

--- src/delivery/handlers/error_handlers.py
+++ src/delivery/handlers/error_handlers.py
@@ -2,8 +2,10 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from sindripy.value_objects import SindriValidationError
 
+from src.delivery.routers.fastapi_response import FastAPIResponse
 from src.shared.infra.http.error_response import (
-    InternalServerError, UnprocessableEntityError,
+    InternalServerError,
+    UnprocessableEntityError,
 )
 
 
@@ -11,11 +13,13 @@ async def unexpected_exception_handler(
     _: Request,
     __: Exception,
 ) -> JSONResponse:
-    return InternalServerError().as_json()
+    return FastAPIResponse.as_json(InternalServerError())
 
 
 async def sindri_validation_error_handler(
     _: Request,
     error: SindriValidationError,
 ) -> JSONResponse:
-    return UnprocessableEntityError(detail={"message": error.message}).as_json()
+    return FastAPIResponse.as_json(
+        UnprocessableEntityError(error={"type": "validation_error", "message": error.message})
+    )
