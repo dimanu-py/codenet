3e057bbfcdc4f709cb0bb4cbc6d0708d61b781d3
dimanu-py <diegomtz126@gmail.com>
2026-02-06 08:40:02 +0100

    feat(shared): add abstract Clock class with now method

---
+++ tests/shared/infra/mock_clock.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from unittest.mock import Mock
+
+from src.shared.domain.clock import Clock
+
+
+class MockClock(Clock):
+    def __init__(self) -> None:
+        self._mock_now = Mock()
+
+    def now(self) -> datetime:
+        self._mock_now.assert_called_with()
+        return self._mock_now.return_value
+
+    def should_generate(self, date: datetime) -> None:
+        self._mock_now.return_value = date
