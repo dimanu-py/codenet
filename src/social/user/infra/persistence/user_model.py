65a701681218f0747ffdcd9e093e8d102139801c
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:19:45 +0100

    refactor(user): update user model to store id as uuid in the database

--- src/social/user/infra/persistence/user_model.py
+++ src/social/user/infra/persistence/user_model.py
@@ -1,6 +1,6 @@
 from typing import Self
 
-from sqlalchemy import String
+from sqlalchemy import String, UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.shared.infra.persistence.sqlalchemy.base import Base
@@ -10,8 +10,8 @@ from src.social.user.domain.user import User
 class UserModel(Base):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(String(36))
-    name: Mapped[str] = mapped_column(String, nullable=False)
+    id: Mapped[str] = mapped_column(UUID(as_uuid=False))
+    name: Mapped[str] = mapped_column(String)
     username: Mapped[str] = mapped_column(String, primary_key=True)
 
     @classmethod
