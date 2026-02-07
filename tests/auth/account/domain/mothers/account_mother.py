a6962d28aa5c169dc0156543b6333d7c2681a422
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:52:08 +0100

    test(account): convert 'any' method to static and add 'create' and 'with_id' methods

--- tests/auth/account/domain/mothers/account_mother.py
+++ tests/auth/account/domain/mothers/account_mother.py
@@ -4,13 +4,15 @@ from src.auth.account.domain.account import Account
 from tests.auth.account.domain.mothers.account_created_at_primitives_mother import AccountCreatedAtPrimitivesMother
 from tests.auth.account.domain.mothers.account_email_primitives_mother import AccountEmailPrimitivesMother
 from tests.auth.account.domain.mothers.account_id_primitives_mother import AccountIdPrimitivesMother
-from tests.auth.account.domain.mothers.account_password_hash_primitives_mother import AccountPasswordHashPrimitivesMother
+from tests.auth.account.domain.mothers.account_password_hash_primitives_mother import (
+    AccountPasswordHashPrimitivesMother,
+)
 from tests.auth.account.domain.mothers.account_status_primitives_mother import AccountStatusPrimitivesMother
 
 
 class AccountMother(ObjectMother):
-    @classmethod
-    def any(cls) -> Account:
+    @staticmethod
+    def any() -> Account:
         return Account(
             id=AccountIdPrimitivesMother.any(),
             email=AccountEmailPrimitivesMother.any(),
@@ -18,3 +20,12 @@ class AccountMother(ObjectMother):
             status=AccountStatusPrimitivesMother.active(),
             created_at=AccountCreatedAtPrimitivesMother.any(),
         )
+
+    @classmethod
+    def create(cls, **overrides) -> Account:
+        default = cls.any().to_primitives()
+        return Account(**{**default, **overrides})
+
+    @classmethod
+    def with_id(cls, id: str) -> Account:
+        return cls.create(id=id)
