9605e6cdd2d0f6d463758338a9b45f955d1aa8ad
dimanu-py <diegomtz126@gmail.com>
2026-02-07 20:33:26 +0100

    feat(account): integrate password hasher with account signup use case to ensure stored password is hashed and secure

--- src/auth/account/application/signup/account_with_user_signup.py
+++ src/auth/account/application/signup/account_with_user_signup.py
@@ -20,25 +20,28 @@ class AccountWithUserSignup:
         email: str,
         plain_password: str,
     ) -> None:
+        hashed_password = self._hash_account_password(plain_password)
         await self._signup_account_with(
             account_id=account_id,
             email=email,
-            plain_password=plain_password,
+            password=hashed_password,
         )
         await self._signup_user_with(
             user_id=account_id,
             name=name,
             username=username,
-            email=email,
         )
 
-    async def _signup_user_with(self, user_id: str, name: str, username: str, email: str) -> None:
+    def _hash_account_password(self, password: str) -> str:
+        return self._password_manager.hash(password)
+
+    async def _signup_user_with(self, user_id: str, name: str, username: str) -> None:
         await self._user_signup.execute(
             id=user_id,
             name=name,
             username=username,
         )
 
-    async def _signup_account_with(self, account_id: str, email: str, plain_password: str) -> None:
-        account = Account.signup(id=account_id, email=email, password=plain_password, clock=self._clock)
+    async def _signup_account_with(self, account_id: str, email: str, password: str) -> None:
+        account = Account.signup(id=account_id, email=email, password=password, clock=self._clock)
         await self._repository.save(account)
