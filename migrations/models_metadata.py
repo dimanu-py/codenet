56f4ba16b9a8bd41c16459153ed39f2b7681c63c
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:18:03 +0100

    feat(migrations): create accounts table with necessary fields and constraints

--- migrations/models_metadata.py
+++ migrations/models_metadata.py
@@ -5,10 +5,11 @@ When we import Base.metadata in the env.py file it will include all the models t
 have already been imported. To keep the env.py file clean, we import the Base.metadata in this file and then import all the
 needed models.
 """
-
+from src.auth.account.infra.persistence.account_model import AccountModel
 from src.shared.infra.persistence.sqlalchemy.base import Base
 from src.social.user.infra.persistence.user_model import UserModel
 
 base = Base
 
 _ = UserModel
+_ = AccountModel
