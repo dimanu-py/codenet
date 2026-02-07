c37780ed89443065c9395fcc2aa200788db14834
dimanu-py <diegomtz126@gmail.com>
2026-02-07 20:41:20 +0100

    feat(account): implement logic to use password hasher from argon library to hash plain password sent for account

--- src/auth/account/infra/argon_password_manager.py
+++ src/auth/account/infra/argon_password_manager.py
@@ -1,9 +1,17 @@
 from typing import override
 
+from argon2 import PasswordHasher
+
 from src.auth.account.domain.password_manager import PasswordManager
 
 
 class ArgonPasswordManager(PasswordManager):
+    _hasher: PasswordHasher = PasswordHasher(
+        time_cost=2,
+        memory_cost=65536,
+        parallelism=4,
+    )
+
     @override
     def hash(self, plain_password: str) -> str:
-        raise NotImplementedError
+        return self._hasher.hash(plain_password)
