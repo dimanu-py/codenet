3db2a32139aa38758120fd4a5b38317b46c455f1
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:54:23 +0100

    test(user): update postgres repository integration tests to insert the user based on a previous existing account

--- tests/backoffice/user/infra/persistence/test_postgres_user_repository.py
+++ tests/backoffice/user/infra/persistence/test_postgres_user_repository.py
@@ -3,14 +3,14 @@ from expects import be_empty, equal, expect
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from src.shared.domain.criteria.condition.operator import Operator
-from src.shared.domain.value_objects.optional import Optional
 from src.backoffice.user.domain.user import User
 from src.backoffice.user.infra.persistence.postgres_user_repository import (
     PostgresUserRepository,
 )
-from tests.shared.domain.criteria.mothers.criteria_mother import CriteriaMother
+from src.shared.domain.criteria.condition.operator import Operator
+from src.shared.domain.value_objects.optional import Optional
 from tests.backoffice.user.domain.mothers.user_mother import UserMother
+from tests.shared.domain.criteria.mothers.criteria_mother import CriteriaMother
 
 
 @pytest.mark.integration
@@ -20,16 +20,16 @@ class TestPostgresUserRepository:
     def setup_method(self, session: AsyncSession) -> None:
         self._repository = PostgresUserRepository(session)
 
-    async def test_should_save_and_find_stored_user(self) -> None:
-        user = UserMother.any()
+    async def test_should_save_and_find_stored_user(self, existing_account_id: str) -> None:
+        user = UserMother.with_id(existing_account_id)
 
         await self._repository.save(user)
         saved_user = await self._repository.search(user.username)
 
         expect(user).to(equal(saved_user.unwrap()))
 
-    async def test_should_match_an_existing_user_based_on_criteria(self) -> None:
-        user = await self._given_an_user_already_exists()
+    async def test_should_match_an_existing_user_based_on_criteria(self, existing_account_id: str) -> None:
+        user = await self._given_an_user_already_exists(existing_account_id)
         criteria = CriteriaMother.with_one_condition(
             field="username", operator=Operator.EQUAL, value=user.username.value
         )
@@ -45,8 +45,8 @@ class TestPostgresUserRepository:
 
         expect(searched_users).to(be_empty)
 
-    async def test_should_delete_existing_user(self) -> None:
-        user = await self._given_an_user_already_exists()
+    async def test_should_delete_existing_user(self, existing_account_id: str) -> None:
+        user = await self._given_an_user_already_exists(existing_account_id)
 
         await self._repository.delete(user.username)
 
@@ -62,7 +62,7 @@ class TestPostgresUserRepository:
         saved_user = await self._repository.search(user.username)
         expect(saved_user).to(equal(Optional.empty()))
 
-    async def _given_an_user_already_exists(self) -> User:
-        user = UserMother.any()
+    async def _given_an_user_already_exists(self, user_id: str) -> User:
+        user = UserMother.with_id(user_id)
         await self._repository.save(user)
         return user
