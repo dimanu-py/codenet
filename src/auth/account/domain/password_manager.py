be8ce623cada73945e107b612088b04360b7ec31
dimanu-py <diegomtz126@gmail.com>
2026-02-07 20:42:58 +0100

    refactor(account): convert password manager into an interface to enforce dependency inversion

--- src/auth/account/domain/password_manager.py
+++ src/auth/account/domain/password_manager.py
@@ -1,3 +1,7 @@
-class PasswordManager:
+from abc import ABC, abstractmethod
+
+
+class PasswordManager(ABC):
+    @abstractmethod
     def hash(self, plain_password: str) -> str:
         raise NotImplementedError
