0955cee5ad65c89d89ce74ed1f40a99bf9723b76
dimanu-py <diegomtz126@gmail.com>
2026-02-07 21:15:57 +0100

    test(account): use AccountPasswordHashPrimitivesMother for password handling in account mother

--- tests/auth/account/domain/mothers/account_mother.py
+++ tests/auth/account/domain/mothers/account_mother.py
@@ -3,6 +3,7 @@ from sindripy.mothers import ObjectMother
 from src.auth.account.domain.account import Account
 from tests.auth.account.domain.mothers.account_email_primitives_mother import AccountEmailPrimitivesMother
 from tests.auth.account.domain.mothers.account_id_primitives_mother import AccountIdPrimitivesMother
+from tests.auth.account.domain.mothers.account_password_hash_primitives_mother import AccountPasswordHashPrimitivesMother
 from tests.auth.account.domain.mothers.account_status_primitives_mother import AccountStatusPrimitivesMother
 
 
@@ -12,7 +13,7 @@ class AccountMother(ObjectMother):
         return Account(
             id=AccountIdPrimitivesMother.any(),
             email=AccountEmailPrimitivesMother.any(),
-            password=cls._faker().password(),
+            password=AccountPasswordHashPrimitivesMother.any(),
             status=AccountStatusPrimitivesMother.active(),
             created_at=cls._faker().date_time(),
         )
