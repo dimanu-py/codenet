ebba99a8040c3b3af8d4503ad9ed3905dd2de3c5
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:52:31 +0100

    test(account): update account fixtures to insert an account and only return the id

--- tests/auth/account/account_fixtures.py
+++ tests/auth/account/account_fixtures.py
@@ -1,14 +1,15 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth.account.domain.account import Account
 from src.auth.account.infra.persistence.account_model import AccountModel
+from tests.auth.account.domain.mothers.account_id_primitives_mother import AccountIdPrimitivesMother
 from tests.auth.account.domain.mothers.account_mother import AccountMother
 
 
 @pytest.fixture
-async def existing_account(session: AsyncSession) -> Account:
-    account = AccountMother.any()
+async def existing_account_id(session: AsyncSession) -> str:
+    account_id = AccountIdPrimitivesMother.any()
+    account = AccountMother.with_id(account_id)
     session.add(AccountModel.from_domain(account))
     await session.commit()
-    return account
+    return account_id
