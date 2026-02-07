a21a64e82d22702ac4284f02a87084e7daa3ba95
dimanu-py <diegomtz126@gmail.com>
2026-02-08 00:40:08 +0100

    feat(user): add foreign key relationship to account

--- src/backoffice/user/infra/persistence/user_model.py
+++ src/backoffice/user/infra/persistence/user_model.py
@@ -1,16 +1,16 @@
 from typing import Self
 
-from sqlalchemy import String, UUID
+from sqlalchemy import String, UUID, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.shared.infra.persistence.sqlalchemy.base import Base
 from src.backoffice.user.domain.user import User
+from src.shared.infra.persistence.sqlalchemy.base import Base
 
 
 class UserModel(Base):
     __tablename__ = "users"
 
-    id: Mapped[str] = mapped_column(UUID(as_uuid=False))
+    id: Mapped[str] = mapped_column(UUID(as_uuid=False), ForeignKey("accounts.id"))
     name: Mapped[str] = mapped_column(String)
     username: Mapped[str] = mapped_column(String, primary_key=True)
 
