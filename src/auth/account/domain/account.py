640a80634abe7f81ca6d534050c60cd0970dcf31
dimanu-py <diegomtz126@gmail.com>
2026-02-07 21:15:35 +0100

    refactor(account): integrate AccountStatus value object for account status handling

--- src/auth/account/domain/account.py
+++ src/auth/account/domain/account.py
@@ -4,6 +4,7 @@ from typing import Self
 from src.auth.account.domain.account_email import AccountEmail
 from src.auth.account.domain.account_id import AccountId
 from src.auth.account.domain.account_password_hash import AccountPasswordHash
+from src.auth.account.domain.account_status import AccountStatus
 from src.shared.domain.clock import Clock
 from src.shared.domain.value_objects.aggregate import Aggregate
 
@@ -13,7 +14,7 @@ class Account(Aggregate):
         self._id = AccountId(id)
         self._email = AccountEmail(email)
         self._password = AccountPasswordHash(password)
-        self._status = status
+        self._status = AccountStatus(status)
         self._created_at = created_at
 
     @classmethod
--- tests/auth/account/domain/mothers/account_mother.py
+++ tests/auth/account/domain/mothers/account_mother.py
@@ -3,6 +3,7 @@ from sindripy.mothers import ObjectMother
 from src.auth.account.domain.account import Account
 from tests.auth.account.domain.mothers.account_email_primitives_mother import AccountEmailPrimitivesMother
 from tests.auth.account.domain.mothers.account_id_primitives_mother import AccountIdPrimitivesMother
+from tests.auth.account.domain.mothers.account_status_primitives_mother import AccountStatusPrimitivesMother
 
 
 class AccountMother(ObjectMother):
@@ -12,6 +13,6 @@ class AccountMother(ObjectMother):
             id=AccountIdPrimitivesMother.any(),
             email=AccountEmailPrimitivesMother.any(),
             password=cls._faker().password(),
-            status="active",
+            status=AccountStatusPrimitivesMother.active(),
             created_at=cls._faker().date_time(),
         )
