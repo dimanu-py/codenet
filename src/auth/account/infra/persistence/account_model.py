52c6c595fe2e75da6daa2839a3c5df68da17c230
dimanu-py <diegomtz126@gmail.com>
2026-02-07 23:57:02 +0100

    feat(account): add AccountModel for account table representation in database

---
+++ src/auth/account/infra/persistence/account_model.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import Self
+
+from sqlalchemy import DateTime, String, UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.auth.account.domain.account import Account
+from src.shared.infra.persistence.sqlalchemy.base import Base
+
+
+class AccountModel(Base):
+    __tablename__ = "accounts"
+
+    id: Mapped[str] = mapped_column(UUID(as_uuid=False), primary_key=True)
+    email: Mapped[str] = mapped_column(String)
+    password: Mapped[str] = mapped_column(String)
+    status: Mapped[str] = mapped_column(String)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+    @classmethod
+    def from_domain(cls, account: Account) -> Self:
+        return cls(**account.to_primitives())
