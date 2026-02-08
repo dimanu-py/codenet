4e626ddef1ee2e6f6fa6366f0ebc590c3eb52296
dimanu-py <diegomtz126@gmail.com>
2026-02-08 19:52:37 +0100

    test(account): implement pre condition setter and search_by_email methods in account mock repository

--- tests/auth/account/infra/persistence/mock_account_repository.py
+++ tests/auth/account/infra/persistence/mock_account_repository.py
@@ -9,12 +9,17 @@ from src.shared.domain.value_objects.optional import Optional
 class MockAccountRepository(AccountRepository):
     def __init__(self) -> None:
         self._mock_save = AsyncMock()
+        self._mock_search = AsyncMock()
 
     async def save(self, account: Account) -> None:
         await self._mock_save(account)
 
     async def search_by_email(self, email: AccountEmail) -> Optional[Account]:
-        pass
+        await self._mock_search(email)
+        return self._mock_search.return_value
 
     def should_have_saved(self, account: Account) -> None:
         self._mock_save.assert_awaited_once_with(account)
+
+    def should_search(self, account: Account) -> None:
+        self._mock_search.return_value = Optional.of(account)
