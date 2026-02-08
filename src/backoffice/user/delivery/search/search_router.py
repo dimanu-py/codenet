063e86b3ef590c1334f4940518e4309b39b096ca
dimanu-py <diegomtz126@gmail.com>
2026-02-08 01:16:32 +0100

    feat(user): enhance user search filter examples in API documentation

--- src/backoffice/user/delivery/search/search_router.py
+++ src/backoffice/user/delivery/search/search_router.py
@@ -4,12 +4,12 @@ from fastapi import APIRouter, Depends, Query, status
 from fastapi.openapi.models import Example
 from fastapi.responses import JSONResponse
 
-from src.shared.delivery.fastapi_response import FastAPIResponse
-from src.shared.infra.http.success_response import OkResponse
 from src.backoffice.user.application.search.user_searcher import UserSearcher
 from src.backoffice.user.delivery.deps import postgres_user_repository
 from src.backoffice.user.domain.user_repository import UserRepository
 from src.backoffice.user.infra.api.search.user_search_controller import UserSearchController
+from src.shared.delivery.fastapi_response import FastAPIResponse
+from src.shared.infra.http.success_response import OkResponse
 
 router = APIRouter()
 
@@ -27,7 +27,12 @@ def get_controller(
     },
 )
 async def get_user_by_criteria(
-    filter: str = Query(openapi_examples={"simple_filter": Example(value='{"field": "username", "equal": "johndoe"}')}),
+    filter: str = Query(
+        openapi_examples={
+            "empty_filter": Example(value="{}"),
+            "simple_filter": Example(value='{"field": "username", "equal": "johndoe"}'),
+        }
+    ),
     controller: UserSearchController = Depends(get_controller),
 ) -> JSONResponse:
     result = await controller.search(filters=json.loads(filter))
