19357a602067c70426643dec6b6ace7de7ee2f4f
dimanu-py <diegomtz126@gmail.com>
2026-02-08 01:14:49 +0100

    fix(user): update user removal endpoint to use username instead of user ID

--- src/backoffice/user/delivery/removal/removal_router.py
+++ src/backoffice/user/delivery/removal/removal_router.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Depends, Path, status
 from fastapi.openapi.models import Example
 from fastapi.responses import JSONResponse
 
-from src.shared.delivery.fastapi_response import FastAPIResponse
-from src.shared.infra.http.error_response import ResourceNotFoundError, UnprocessableEntityError
-from src.shared.infra.http.success_response import AcceptedResponse
 from src.backoffice.user.application.removal.user_remover import UserRemover
 from src.backoffice.user.delivery.deps import postgres_user_repository
 from src.backoffice.user.domain.user_repository import UserRepository
 from src.backoffice.user.infra.api.removal.user_removal_controller import UserRemovalController
+from src.shared.delivery.fastapi_response import FastAPIResponse
+from src.shared.infra.http.error_response import ResourceNotFoundError, UnprocessableEntityError
+from src.shared.infra.http.success_response import AcceptedResponse
 
 router = APIRouter()
 
@@ -18,7 +18,7 @@ def get_controller(repository: UserRepository = Depends(postgres_user_repository
 
 
 @router.delete(
-    "/{user_id}",
+    "/{user_username}",
     responses={
         status.HTTP_202_ACCEPTED: {"model": AcceptedResponse},
         status.HTTP_404_NOT_FOUND: {"model": ResourceNotFoundError},
@@ -26,8 +26,8 @@ def get_controller(repository: UserRepository = Depends(postgres_user_repository
     },
 )
 async def remove_user(
-    user_id: str = Path(..., openapi_examples={"valid_id": Example(value="123e4567-e89b-12d3-a456-426614174000")}),
+    user_username: str = Path(..., openapi_examples={"valid_username": Example(value="johndoe")}),
     controller: UserRemovalController = Depends(get_controller),
 ) -> JSONResponse:
-    result = await controller.remove(username=user_id)
+    result = await controller.remove(username=user_username)
     return FastAPIResponse.as_json(result)
