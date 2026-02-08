305ca45156798ebf35d29088e79128cbc402b1d0
dimanu-py <diegomtz126@gmail.com>
2026-02-08 19:31:50 +0100

    refactor(account): catch conflict error in signup controller to be able to return a controlled error to the client

--- src/auth/account/infra/api/signup/signup_controller.py
+++ src/auth/account/infra/api/signup/signup_controller.py
@@ -1,4 +1,6 @@
 from src.auth.account.application.signup.account_with_user_signup import AccountWithUserSignup
+from src.shared.domain.exceptions.application_error import ConflictError
+from src.shared.infra.http.error_response import ConflictErrorResponse, ErrorResponse
 from src.shared.infra.http.success_response import AcceptedResponse, SuccessResponse
 
 
@@ -6,12 +8,15 @@ class SignupController:
     def __init__(self, use_case: AccountWithUserSignup) -> None:
         self._signup = use_case
 
-    async def signup(self, account_id: str, name: str, username: str, email: str, password: str) -> SuccessResponse:
-        await self._signup.execute(
-            account_id=account_id,
-            name=name,
-            username=username,
-            email=email,
-            plain_password=password,
-        )
+    async def signup(self, account_id: str, name: str, username: str, email: str, password: str) -> SuccessResponse | ErrorResponse:
+        try:
+            await self._signup.execute(
+                account_id=account_id,
+                name=name,
+                username=username,
+                email=email,
+                plain_password=password,
+            )
+        except ConflictError as error:
+            return ConflictErrorResponse(error=error.to_primitives())
         return AcceptedResponse()
