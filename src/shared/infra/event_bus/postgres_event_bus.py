39fb85e2aa3e9f04e0f23a7880b3e853758a077d
dimanu-py <diegomtz126@gmail.com>
2026-02-10 15:36:02 +0100

    feat(shared): implement publish method in PostgresEventBus to handle event consumption

--- src/shared/infra/event_bus/postgres_event_bus.py
+++ src/shared/infra/event_bus/postgres_event_bus.py
@@ -1,14 +1,15 @@
-from collections.abc import Iterable
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.shared.domain.event_bus.domain_event import DomainEvent
 from src.shared.domain.event_bus.event_bus import EventBus
+from src.shared.infra.event_bus.domain_event_to_consume_model import DomainEventToConsumeModel
 
 
 class PostgresEventBus(EventBus):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
 
-    async def publish(self, events: Iterable[DomainEvent]) -> None:
-        raise NotImplementedError
+    async def publish(self, events: list[DomainEvent]) -> None:
+        events_to_publish = [DomainEventToConsumeModel.from_domain(event) for event in events]
+        self._session.add_all(events_to_publish)
+        await self._session.commit()
