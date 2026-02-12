6a68309aefcc46c472989e30a4b1c0ebc464b909
dimanu-py <diegomtz126@gmail.com>
2026-02-12 07:54:31 +0100

    refactor(shared): use session factory for async session management and remove redundant session.close() sentence

--- src/shared/delivery/db_session.py
+++ src/shared/delivery/db_session.py
@@ -1,21 +1,20 @@
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from src.shared.delivery.settings import Settings
 
 settings = Settings()  # type: ignore
 engine = create_async_engine(str(settings.postgres_url))
+session_factory = async_sessionmaker(bind=engine, expire_on_commit=False, autoflush=False)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession]:
-    async with AsyncSession(engine, expire_on_commit=False, autoflush=False) as session:
+    async with session_factory() as session:
         try:
             yield session
             await session.commit()
         except Exception:
             await session.rollback()
             raise
-        finally:
-            await session.close()
