206ecc9df2a833fdb156df4695b9ee31602b748b
dimanu-py <diegomtz126@gmail.com>
2026-02-13 22:35:43 +0100

    refactor(user): update filter query param with annotated type

--- src/backoffice/user/delivery/search/search_router.py
+++ src/backoffice/user/delivery/search/search_router.py
@@ -1,17 +1,29 @@
 import json
+from typing import Annotated
 
-from fastapi import APIRouter, Depends, Query, status
-from fastapi.openapi.models import Example
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 
 from src.backoffice.user.application.search.user_searcher import UserSearcher
 from src.backoffice.user.delivery.deps import postgres_user_repository
 from src.backoffice.user.domain.user_repository import UserRepository
 from src.backoffice.user.infra.api.search.user_search_controller import UserSearchController
+from src.shared.delivery.api_parameter import QueryParameter, ApiDocExample
 from src.shared.delivery.fastapi_response import FastAPIResponse
 from src.shared.infra.api.success_response import OkResponse
 
 router = APIRouter()
+
+UserFilterQueryParameter = Annotated[
+    str,
+    QueryParameter(
+        description="Filter in JSON format",
+        examples=[
+            ApiDocExample(name="empty_filter", value="{}"),
+            ApiDocExample(name="simple_filter", value='{"field": "username", "equal": "johndoe"}'),
+        ],
+    ),
+]
 
 
 def get_controller(
@@ -27,12 +39,7 @@ def get_controller(
     },
 )
 async def get_user_by_criteria(
-    filter: str = Query(
-        openapi_examples={
-            "empty_filter": Example(value="{}"),
-            "simple_filter": Example(value='{"field": "username", "equal": "johndoe"}'),
-        }
-    ),
+    filter: UserFilterQueryParameter,
     controller: UserSearchController = Depends(get_controller),
 ) -> JSONResponse:
     result = await controller.search(filters=json.loads(filter))
