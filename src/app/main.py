494410401fcc34e925de50cb14eacb5f66aa0e1d
dimanu-py <diegomtz126@gmail.com>
2026-02-14 00:08:39 +0100

    refactor(app): split main application creation in two phases, one creates the basic app that will be used by tests and the second phase configures the app for production including di container, logg and middlewares

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,8 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from asgi_correlation_id import CorrelationIdMiddleware
+from dishka import make_async_container
+from dishka.integrations.fastapi import setup_dishka
 from fastapi import FastAPI
 from sindripy.value_objects import SindriValidationError
 
@@ -11,6 +13,7 @@ from src.app.handlers.error_handlers import (
     unexpected_exception_handler,
 )
 from src.app.middleware.fast_api_log_middleware import FastapiLogMiddleware
+from src.auth.account.infra.injector.account_dependency_provider import AccountDependencyProvider
 from src.auth.routes import auth_routes
 from src.backoffice.routes import social_routes
 from src.shared.infra.logger.fastapi_file_logger import (
@@ -25,15 +28,26 @@ async def lifespan(_: FastAPI) -> AsyncGenerator[None]:
     yield
 
 
-logger = create_api_logger(name="codenet")
+def create_app() -> FastAPI:
+    basic_app = FastAPI(lifespan=lifespan)
+    basic_app.include_router(social_routes)
+    basic_app.include_router(auth_routes)
+    basic_app.add_exception_handler(Exception, unexpected_exception_handler)
+    basic_app.add_exception_handler(SindriValidationError, sindri_validation_error_handler)
+    return basic_app
 
-app = FastAPI(lifespan=lifespan)
 
-app.add_middleware(FastapiLogMiddleware, logger=logger)  # type: ignore
-app.add_middleware(CorrelationIdMiddleware)  # type: ignore
+def create_production_app() -> FastAPI:
+    production_app = create_app()
 
-app.add_exception_handler(Exception, unexpected_exception_handler)
-app.add_exception_handler(SindriValidationError, sindri_validation_error_handler)
+    di_container = make_async_container(AccountDependencyProvider())
+    setup_dishka(di_container, production_app)
 
-app.include_router(social_routes)
-app.include_router(auth_routes)
+    logger = create_api_logger(name="codenet")
+    production_app.add_middleware(FastapiLogMiddleware, logger=logger)  # type: ignore
+    production_app.add_middleware(CorrelationIdMiddleware)  # type: ignore
+
+    return production_app
+
+
+app = create_production_app()
