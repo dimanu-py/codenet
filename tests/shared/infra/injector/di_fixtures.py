59e7c9d52b12f95729d7169a1006dd58619c3f37
dimanu-py <diegomtz126@gmail.com>
2026-02-14 20:07:37 +0100

    test(shared): update di fixture to use same auto-discovery logic for containers

--- tests/shared/infra/injector/di_fixtures.py
+++ tests/shared/infra/injector/di_fixtures.py
@@ -4,15 +4,13 @@ import pytest
 from dishka import AsyncContainer, make_async_container
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.auth.account.infra.injector.account_dependency_provider import AccountDependencyProvider
-from src.backoffice.user.infra.injector.user_dependency_provider import UserDependencyProvider
+from src.shared.infra.injector.registry import register_providers, get_registered_providers
 from tests.shared.infra.injector.test_database_session_provider import TestDatabaseSessionProvider
 
 
 @pytest.fixture
 def container(session: AsyncSession) -> Generator[AsyncContainer]:
-    container = make_async_container(
-        TestDatabaseSessionProvider(session), AccountDependencyProvider(), UserDependencyProvider()
-    )
+    register_providers()
+    container = make_async_container(*get_registered_providers(), TestDatabaseSessionProvider(session))
     yield container
     container.close()
