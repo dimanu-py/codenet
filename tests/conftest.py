6a9d08dd95e3e9b4ddb45005056ab2ba352cef29
dimanu-py <diegomtz126@gmail.com>
2026-02-14 16:35:10 +0100

    test: adapt test application to be able to use session with testcontainer for postgres and apply dependency injector correctly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Generator
 
 import pytest
+from dishka import Provider, Scope, make_async_container, provide, AsyncContainer
+from dishka.integrations.fastapi import setup_dishka
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from src.app.main import app
-from src.shared.delivery.db_session import get_async_session
+from src.app.main import create_app
+from src.auth.account.infra.injector.account_dependency_provider import AccountDependencyProvider
+from src.backoffice.user.infra.injector.user_dependency_provider import UserDependencyProvider
 
 pytest_plugins = [
     "tests.shared.infra.database_fixtures",
@@ -14,14 +17,31 @@ pytest_plugins = [
 ]
 
 
-@pytest.fixture
-async def client(session: AsyncSession) -> AsyncGenerator[AsyncClient]:
-    def get_override_session() -> AsyncSession:
-        return session
+class TestSessionProvider(Provider):
+    scope = Scope.REQUEST
 
-    app.dependency_overrides[get_async_session] = get_override_session
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__()
+        self._session = session
+
+    @provide
+    def session(self) -> AsyncSession:
+        return self._session
+
+
+@pytest.fixture
+def container(session: AsyncSession) -> Generator[AsyncContainer]:
+    container = make_async_container(
+        TestSessionProvider(session), AccountDependencyProvider(), UserDependencyProvider()
+    )
+    yield container
+    container.close()
+
+
+@pytest.fixture
+async def client(container, session: AsyncSession) -> AsyncGenerator[AsyncClient]:
+    app = create_app()
+    setup_dishka(container, app)
 
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
-
-    app.dependency_overrides.clear()
