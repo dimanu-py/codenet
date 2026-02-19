b26875af91a68b3125a5c046196fd15f4afda3cb
dimanu-py <diegomtz126@gmail.com>
2026-02-19 09:44:19 +0100

    feat(account): add AccountUsername value object with validation for username format

---
+++ src/auth/account/domain/account_username.py
@@ -0,0 +1,24 @@
+import re
+
+from src.shared.domain.exceptions.domain_error import DomainValidationError
+from src.shared.domain.value_objects.string_value_object import (
+    StringValueObject,
+)
+from src.shared.domain.value_objects.validation import validate
+
+
+class AccountUsername(StringValueObject):
+    CORRECT_CHARACTERS = r"^[a-zA-Z0-9_.]+$"
+
+    @validate
+    def _ensure_name_has_valid_characters(self) -> None:
+        if re.match(self.CORRECT_CHARACTERS, self._value) is None:
+            raise InvalidUsernameFormat
+
+
+class InvalidUsernameFormat(DomainValidationError):
+    def __init__(self) -> None:
+        super().__init__(
+            message="Username cannot contain special characters",
+            error_type="account_validation_error",
+        )
