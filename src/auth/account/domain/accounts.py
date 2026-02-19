b022db26f3cf831abc0011d451b56631e45ec0bf
dimanu-py <diegomtz126@gmail.com>
2026-02-19 08:34:58 +0100

    feat(account): add iterable and equality comparison methods to Accounts class

--- src/auth/account/domain/accounts.py
+++ src/auth/account/domain/accounts.py
@@ -1,6 +1,20 @@
+from collections.abc import Iterable
+from typing import Self
+
 from src.auth.account.domain.account import Account
 
 
 class Accounts:
     def __init__(self, accounts: list[Account]) -> None:
         self._accounts = accounts
+
+    def __iter__(self)-> Iterable[Account]:
+        return iter(self._accounts)
+
+    def __len__(self) -> int:
+        return len(self._accounts)
+
+    def __eq__(self, other: Self) -> bool:
+        if not isinstance(other, Accounts):
+            return False
+        return self._accounts == other._accounts
