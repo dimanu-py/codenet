c52c7570e9af2084dafb17dee1ad925b0a0f62ff
dimanu-py <diegomtz126@gmail.com>
2026-02-20 16:54:39 +0100

    feat(account): add authenticate controller to account dependency provider

--- src/auth/account/infra/injector/account_dependency_provider.py
+++ src/auth/account/infra/injector/account_dependency_provider.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.account.application.signup.account_signup import AccountSignup
 from src.auth.account.domain.account_repository import AccountRepository
+from src.auth.account.infra.api.authenticate.authenticate_account_controller import AuthenticateAccountController
 from src.auth.account.infra.api.signup.signup_account_controller import SignupAccountController
 from src.auth.account.infra.argon_password_manager import ArgonPasswordManager
 from src.auth.account.infra.persistence.postgres_account_repository import PostgresAccountRepository
@@ -24,3 +25,7 @@ class AccountDependencyProvider(Provider):
         clock = DatetimeClock()
         use_case = AccountSignup(repository=account_repository, password_manager=password_manager, clock=clock)
         return SignupAccountController(use_case=use_case)
+
+    @provide
+    def authenticate_controller(self) -> AuthenticateAccountController:
+        return AuthenticateAccountController()
