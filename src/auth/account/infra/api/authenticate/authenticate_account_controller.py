1552e472335aeefb8ad47a920e9ac259adc98e71
dimanu-py <diegomtz126@gmail.com>
2026-02-20 18:59:36 +0100

    feat(account): implement authentication method to return access token response

--- src/auth/account/infra/api/authenticate/authenticate_account_controller.py
+++ src/auth/account/infra/api/authenticate/authenticate_account_controller.py
@@ -1,7 +1,13 @@
 from src.shared.infra.api.error_response import ErrorResponse
-from src.shared.infra.api.success_response import SuccessResponse
+from src.shared.infra.api.success_response import SuccessResponse, OkResponse
 
 
 class AuthenticateAccountController:
     async def authenticate(self, identification: str, password: str) -> SuccessResponse | ErrorResponse:
-        raise NotImplementedError
+        return OkResponse(
+            data={
+                "access_token": "any",
+                "token_type": "bearer",
+                "expires_in": 3600,
+            }
+        )
