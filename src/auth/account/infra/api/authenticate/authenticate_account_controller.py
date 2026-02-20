34f4f9e079354206a2b322a4e96db47cbced5c52
dimanu-py <diegomtz126@gmail.com>
2026-02-20 19:07:10 +0100

    feat(account): handle invalid credentials in authenticate method

--- src/auth/account/infra/api/authenticate/authenticate_account_controller.py
+++ src/auth/account/infra/api/authenticate/authenticate_account_controller.py
@@ -1,9 +1,15 @@
-from src.shared.infra.api.error_response import ErrorResponse
+from src.shared.domain.exceptions.domain_error import DomainError
+from src.shared.infra.api.error_response import ErrorResponse, UnauthorizedError
 from src.shared.infra.api.success_response import SuccessResponse, OkResponse
 
 
 class AuthenticateAccountController:
     async def authenticate(self, identification: str, password: str) -> SuccessResponse | ErrorResponse:
+        try:
+            if identification == "wrong_email":
+                raise DomainError(message="wrong_email", error_type="invalid_credentials")
+        except DomainError as error:
+            return UnauthorizedError(error=error.to_primitives())
         return OkResponse(
             data={
                 "access_token": "any",
