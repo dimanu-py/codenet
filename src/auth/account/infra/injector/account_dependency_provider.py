4cea581906b37afc83acbbc7a24424dda702ac0c
dimanu-py <diegomtz126@gmail.com>
2026-02-20 16:51:54 +0100

    refactor(account): simplify account dependency provider by removing unnecessary providers and streamline them inside signup controller

--- src/auth/account/infra/injector/account_dependency_provider.py
+++ src/auth/account/infra/injector/account_dependency_provider.py
@@ -3,11 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.auth.account.application.signup.account_signup import AccountSignup
 from src.auth.account.domain.account_repository import AccountRepository
-from src.auth.account.domain.password_manager import PasswordManager
 from src.auth.account.infra.api.signup.signup_account_controller import SignupAccountController
 from src.auth.account.infra.argon_password_manager import ArgonPasswordManager
 from src.auth.account.infra.persistence.postgres_account_repository import PostgresAccountRepository
-from src.shared.domain.clock import Clock
 from src.shared.infra.datetime_clock import DatetimeClock
 from src.shared.infra.injector.registry import register_provider
 
@@ -21,19 +19,8 @@ class AccountDependencyProvider(Provider):
         return PostgresAccountRepository(session=session)
 
     @provide
-    def password_manager(self) -> PasswordManager:
-        return ArgonPasswordManager()
-
-    @provide
-    def clock(self) -> Clock:
-        return DatetimeClock()
-
-    @provide
-    def account_signup(
-        self, repository: AccountRepository, password_manager: PasswordManager, clock: Clock
-    ) -> AccountSignup:
-        return AccountSignup(repository=repository, password_manager=password_manager, clock=clock)
-
-    @provide
-    def signup_controller(self, use_case: AccountSignup) -> SignupAccountController:
+    def signup_controller(self, account_repository: AccountRepository) -> SignupAccountController:
+        password_manager = ArgonPasswordManager()
+        clock = DatetimeClock()
+        use_case = AccountSignup(repository=account_repository, password_manager=password_manager, clock=clock)
         return SignupAccountController(use_case=use_case)
