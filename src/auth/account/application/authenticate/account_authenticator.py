0ebb45cf3df3a7c99a5fe3b980e2005a45d6e630
dimanu-py <diegomtz126@gmail.com>
2026-02-21 20:14:08 +0100

    feat(account): add InvalidCredentials exception for handling authentication errors

--- src/auth/account/application/authenticate/account_authenticator.py
+++ src/auth/account/application/authenticate/account_authenticator.py
@@ -1,3 +1,14 @@
+from src.shared.domain.exceptions.domain_error import DomainError
+
+
 class AccountAuthenticator:
     async def execute(self, identification: str, password: str) -> str:
         raise NotImplementedError
+
+
+class InvalidCredentials(DomainError):
+    def __init__(self) -> None:
+        super().__init__(
+            message="Invalid credentials",
+            error_type="invalid_credentials",
+        )
