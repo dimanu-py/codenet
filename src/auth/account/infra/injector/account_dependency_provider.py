68f7dc6c35afe48bb630253e76d359f908fca1c4
dimanu-py <diegomtz126@gmail.com>
2026-02-23 08:50:20 +0100

    refactor(account): integrate PasswordManager and JwtTokenIssuer into dependency provider

--- src/auth/account/infra/injector/account_dependency_provider.py
+++ src/auth/account/infra/injector/account_dependency_provider.py
@@ -4,9 +4,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.auth.account.application.authenticate.account_authenticator import AccountAuthenticator
 from src.auth.account.application.signup.account_signup import AccountSignup
 from src.auth.account.domain.account_repository import AccountRepository
+from src.auth.account.domain.password_manager import PasswordManager
 from src.auth.account.infra.api.authenticate.authenticate_account_controller import AuthenticateAccountController
 from src.auth.account.infra.api.signup.signup_account_controller import SignupAccountController
 from src.auth.account.infra.argon_password_manager import ArgonPasswordManager
+from src.auth.account.infra.jwt_token_issuer import JwtTokenIssuer
 from src.auth.account.infra.persistence.postgres_account_repository import PostgresAccountRepository
 from src.shared.infra.datetime_clock import DatetimeClock
 from src.shared.infra.injector.registry import register_provider
@@ -21,13 +23,25 @@ class AccountDependencyProvider(Provider):
         return PostgresAccountRepository(session=session)
 
     @provide
-    def signup_controller(self, account_repository: AccountRepository) -> SignupAccountController:
+    def password_manager(self) -> PasswordManager:
+        return ArgonPasswordManager()
+
+    @provide
+    def signup_controller(
+        self, account_repository: AccountRepository, password_manager: PasswordManager
+    ) -> SignupAccountController:
         return SignupAccountController(
             use_case=AccountSignup(
-                repository=account_repository, password_manager=ArgonPasswordManager(), clock=DatetimeClock()
+                repository=account_repository, password_manager=password_manager, clock=DatetimeClock()
             )
         )
 
     @provide
-    def authenticate_controller(self) -> AuthenticateAccountController:
-        return AuthenticateAccountController(use_case=AccountAuthenticator())
+    def authenticate_controller(
+        self, repository: AccountRepository, password_manager: PasswordManager
+    ) -> AuthenticateAccountController:
+        return AuthenticateAccountController(
+            use_case=AccountAuthenticator(
+                repository=repository, password_manager=password_manager, token_issuer=JwtTokenIssuer()
+            )
+        )
