0b36a343806396d49d2544f8e7e7cefa4b55f40f
dimanu-py <diegomtz126@gmail.com>
2026-02-26 18:51:51 +0100

    test(account): modify account password mother to generate a hashed password instead of a plain password
    When generating instances for tests, they were generated with a password that would not match the valid state of the result

--- tests/auth/account/domain/mothers/account_password_hash_primitives_mother.py
+++ tests/auth/account/domain/mothers/account_password_hash_primitives_mother.py
@@ -1,7 +1,15 @@
+from argon2 import PasswordHasher
 from sindripy.mothers import ObjectMother
 
 
 class AccountPasswordHashPrimitivesMother(ObjectMother):
+    _HASHER = PasswordHasher(
+        time_cost=2,
+        memory_cost=65536,
+        parallelism=4,
+    )
+
     @classmethod
     def any(cls) -> str:
-        return cls._faker().password()
+        plain_password = cls._faker().password()
+        return cls._HASHER.hash(plain_password)
