401eb6f44b641f0749307a34c8fdf82e5d35e1b6
dimanu-py <diegomtz126@gmail.com>
2026-03-01 12:32:54 +0100

    test(session): create integration test to verify account credentials are returned when using email for authenticating

--- tests/auth/session/infra/service/test_postgres_account_credentials_finder.py
+++ tests/auth/session/infra/service/test_postgres_account_credentials_finder.py
@@ -1,6 +1,8 @@
 import pytest
+from expects import expect, be_none
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.auth.session.domain.login_identifier import LoginIdentifier
 from src.auth.session.infra.service.postgres_account_credentials_finder import PostgresAccountCredentialsFinder
 
 
@@ -10,7 +12,13 @@ class TestPostgresAccountCredentialsFinder:
     @pytest.fixture(autouse=True)
     def setup_method(self, session: AsyncSession) -> None:
         self._session = session
-        self._repository = PostgresAccountCredentialsFinder(session)
+        self._credentials_finder = PostgresAccountCredentialsFinder(session)
 
-    async def test_should_return_credentials_of_an_existing_account_using_email(self, existing_account_email: str) -> None:
+    async def test_should_return_account_credentials_of_an_existing_account_using_email(
+        self, existing_account_email: str
+    ) -> None:
+        login_identifier = LoginIdentifier(existing_account_email)
 
+        account_auth_credentials = await self._credentials_finder.find_by_login_identifier(login_identifier)
+
+        expect(account_auth_credentials).to_not(be_none)
