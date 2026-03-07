fdff6d78b5f682bd725910c63af5e08e16e9b26c
dimanu-py <diegomtz126@gmail.com>
2026-03-07 20:30:33 +0100

    feat(user): handle invalid search criteria by returning a BadRequestError

--- src/backoffice/user/infra/api/search/user_search_controller.py
+++ src/backoffice/user/infra/api/search/user_search_controller.py
@@ -1,4 +1,6 @@
 from src.backoffice.user.application.search.user_searcher import UserSearcher
+from src.shared.domain.criteria.invalid_criteria import InvalidCriteria
+from src.shared.infra.api.error_response import ErrorResponse, BadRequestError
 from src.shared.infra.api.success_response import OkResponse, SuccessResponse
 
 
@@ -6,7 +8,10 @@ class UserSearchController:
     def __init__(self, use_case: UserSearcher) -> None:
         self._searcher = use_case
 
-    async def search(self, filters: dict) -> SuccessResponse:
-        users = await self._searcher.execute(filters)
+    async def search(self, filters: dict) -> SuccessResponse | ErrorResponse:
+        try:
+            users = await self._searcher.execute(filters)
+        except InvalidCriteria as error:
+            return BadRequestError(error=error.to_primitives())
 
         return OkResponse(data=[user.to_primitives() for user in users])
